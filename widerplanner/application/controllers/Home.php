7bb691dd0f229fafc035eb55e4fc5c835e600250
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-04-13 17:02:44 -0300

    alterando

--- widerplanner/application/controllers/Home.php
+++ widerplanner/application/controllers/Home.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Index extends CI_Controller {
+class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('index');
+		$this->load->view('home');
 	}
 }

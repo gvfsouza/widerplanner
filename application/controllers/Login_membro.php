11404f225a37e398a7d6f4188f336d5a128cded0
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-04-19 16:17:44 -0300

    colocando anti_injection controller

--- application/controllers/Login_membro.php
+++ application/controllers/Login_membro.php
@@ -2,7 +2,18 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_membro extends CI_Controller {
-
+	public function __construct()
+	{
+	  parent::__construct();
+		
+	  if (isset($_POST)) {
+		if (!empty($_POST)) {
+		  anti_injection($_POST);
+		}
+		
+	  }
+	}
+	
 	public function index()
 	{
         $this->load->view('layout/header');

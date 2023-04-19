46d25827e0900c0d5b3e2a444bc488f81e23263a
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-04-19 12:01:38 -0300

    alterando controller servicos

--- application/controllers/Servicos.php
+++ application/controllers/Servicos.php
@@ -2,7 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Servicos extends CI_Controller {
-
+	public function __construct()
+	{
+	  parent::__construct();
+	  //direciona para a página de login quando perde a sessão
+		if($this->session->userdata('codigo_funcionario')==null) redirect('login');
+		
+	  if (isset($_POST)) {
+		if (!empty($_POST)) {
+		  anti_injection($_POST);
+		}
+		
+	  }
+	}
+	
 	public function index()
 	{
 		$this->load->view('servicos');

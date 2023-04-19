02a9e490a221762c589ac53ef7861f2fac5a2d8d
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-04-19 14:12:54 -0300

    alterando header

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="https://www.indaiatuba.sp.gov.br/favicon.ico" type="image/x-icon">
     <link href="public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="public/css/app.css">

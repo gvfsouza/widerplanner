6b4371a1db1fe7bd24abb15871db69133fe8fdad
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-04-19 13:56:29 -0300

    incluindo footer

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,27 @@
+<!-- Footer -->
+</body>
+
+<style>
+    footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
+
+<br><br><br><br><br><br>
+
+<footer class="sticky-footer bg-white">
+
+    <div class="container my-auto">
+        <div class="copyright text-center my-1 mt-1 mb-2 text-muted">
+            <span>Sistema desenvolvido para cunho estudantil.</span>
+        </div>
+        <div class="copyright text-center my-1 mt-1 mb-2 text-muted">
+            <span>Projeto de Conclusão de Curso</span>
+        </div>
+        <div class="copyright text-center my-1 mt-1 mb-2 text-muted">
+            <span>Centro Universitário Nossa Senhora do Patrocínio</span>
+        </div>
+    </div>
+</footer>

05da632cc4d2afd13be7d5b3223874b51190e86e
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-04-22 16:31:24 -0300

    mudando pefil

--- application/views/perfil_cliente.php
+++ application/views/perfil_cliente.php
@@ -15,7 +15,7 @@
                             <div class="col-12">
                                 <fieldset class="form-group"
                                     style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #B98F56;">
-                                    <div>
+                                    <div style="background-color:#B98F56;color:black">
                                         <legend class="form-group col-md-4" style="font-size: 18px; color: #B98F56">
                                             <strong>Dados Pessoais</strong>
                                         </legend>

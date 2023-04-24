70999a0ebc2cc1707bc25443270c7d3ab536b323
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-04-24 12:26:05 -0300

    .

--- application/views/login_membro.php
+++ application/views/login_membro.php
@@ -1,28 +1,7 @@
-<head>
-	<title>Login V1</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-</head>
-
 <style>
 	@font-face {
 		font-family: Poppins-Regular;
 		src: url('../fonts/poppins/Poppins-Regular.ttf');
-	}
-
-	@font-face {
-		font-family: Poppins-Bold;
-		src: url('../fonts/poppins/Poppins-Bold.ttf');
 	}
 
 	@font-face {
@@ -31,13 +10,15 @@
 	}
 
 	@font-face {
-		font-family: Montserrat-Bold;
-		src: url('../fonts/montserrat/Montserrat-Bold.ttf');
+		font-family: Montserrat-Medium;
+		src: url('../fonts/montserrat/Montserrat-Medium.ttf');
 	}
 
-	/*//////////////////////////////////////////////////////////////////
-[ RESTYLE TAG ]*/
-	/*---------------------------------------------*/
+	@font-face {
+		font-family: Montserrat-SemiBold;
+		src: url('../fonts/montserrat/Montserrat-SemiBold.ttf');
+	}
+
 	a {
 		font-family: Poppins-Regular;
 		font-size: 14px;
@@ -54,12 +35,6 @@
 		outline: none !important;
 	}
 
-	a:hover {
-		text-decoration: none;
-		color: #B98F56;
-	}
-
-	/*---------------------------------------------*/
 	h1,
 	h2,
 	h3,
@@ -83,8 +58,6 @@
 		list-style-type: none;
 	}
 
-
-	/*---------------------------------------------*/
 	input {
 		outline: none;
 		border: none;
@@ -133,38 +106,42 @@
 	}
 
 	input::-webkit-input-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	input:-moz-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	input::-moz-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	input:-ms-input-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	textarea::-webkit-input-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	textarea:-moz-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	textarea::-moz-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
 	textarea:-ms-input-placeholder {
-		color: #999999;
+		color: #555555;
 	}
 
-	/*---------------------------------------------*/
+	label {
+		display: block;
+		margin: 0;
+	}
+
 	button {
 		outline: none !important;
 		border: none;
@@ -179,22 +156,26 @@
 		border: none !important;
 	}
 
-	/*Utility */
+	/* Utilidades */
 	.txt1 {
-		font-family: Poppins-Regular;
-		font-size: 13px;
+		font-family: Montserrat-SemiBold;
+		font-size: 16px;
+		color: #555555;
 		line-height: 1.5;
-		color: #999999;
 	}
 
 	.txt2 {
 		font-family: Poppins-Regular;
-		font-size: 13px;
+		font-size: 14px;
+		color: #999999;
 		line-height: 1.5;
-		color: #666666;
 	}
 
-	/* login */
+	.bo1 {
+		border-bottom: 1px solid #999999;
+	}
+
+	/* Login */
 	.limiter {
 		width: 100%;
 		margin: 0 auto;
@@ -212,133 +193,170 @@
 		justify-content: center;
 		align-items: center;
 		padding: 15px;
-		background: #4C4D4F;
+
+		background-position: center;
+		background-size: cover;
+		background-repeat: no-repeat;
+		;
 	}
 
 	.wrap-login100 {
-		width: 960px;
+		width: 680px;
 		background: #fff;
 		border-radius: 10px;
-		overflow: hidden;
-
-		display: -webkit-box;
-		display: -webkit-flex;
-		display: -moz-box;
-		display: -ms-flexbox;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-		padding: 177px 130px 33px 95px;
+		position: relative;
 	}
 
-	/*------------------------------------------------------------------
-[  ]*/
-	.login100-pic {
-		width: 316px;
-	}
-
-	.login100-pic img {
-		max-width: 100%;
-	}
-
+	/* Forms */
 	.login100-form {
-		width: 290px;
+		width: 100%;
 	}
 
 	.login100-form-title {
-		font-family: Poppins-Bold;
-		font-size: 24px;
-		color: #333333;
+		width: 100%;
+		display: block;
+		font-family: Montserrat-Medium;
+		font-size: 39px;
+		color: #555555;
 		line-height: 1.2;
 		text-align: center;
-
-		width: 100%;
-		display: block;
-		padding-bottom: 54px;
 	}
 
-	.wrap-input100 {
-		position: relative;
-		width: 100%;
-		z-index: 1;
-		margin-bottom: 10px;
-	}
-
-	.input100 {
-		font-family: Poppins-Medium;
-		font-size: 15px;
-		line-height: 1.5;
-		color: #666666;
-
-		display: block;
-		width: 100%;
-		background: #e6e6e6;
-		height: 50px;
-		border-radius: 25px;
-		padding: 0 30px 0 68px;
-	}
-
-	/* Focus */
-	.focus-input100 {
-		display: block;
-		position: absolute;
-		border-radius: 25px;
-		bottom: 0;
-		left: 0;
-		z-index: -1;
-		width: 100%;
-		height: 100%;
-		box-shadow: 0px 0px 0px 0px;
-		color: rgba(87, 184, 70, 0.8);
-	}
-
-	.input100:focus+.focus-input100 {
-		-webkit-animation: anim-shadow 0.5s ease-in-out forwards;
-		animation: anim-shadow 0.5s ease-in-out forwards;
-	}
-
-	@-webkit-keyframes anim-shadow {
-		to {
-			box-shadow: 0px 0px 70px 25px;
-			opacity: 0;
-		}
-	}
-
-	@keyframes anim-shadow {
-		to {
-			box-shadow: 0px 0px 70px 25px;
-			opacity: 0;
-		}
-	}
-
-	.symbol-input100 {
-		font-size: 15px;
+	/* Button logar com ... */
+	.btn-face,
+	.btn-google {
+		font-family: Montserrat-SemiBold;
+		font-size: 18px;
+		line-height: 1.2;
 
 		display: -webkit-box;
 		display: -webkit-flex;
 		display: -moz-box;
 		display: -ms-flexbox;
 		display: flex;
+		justify-content: center;
 		align-items: center;
-		position: absolute;
-		border-radius: 25px;
-		bottom: 0;
-		left: 0;
+		padding: 15px;
+		width: calc((100% - 20px) / 2);
+		height: 70px;
+		border-radius: 10px;
+		box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.2);
+		-moz-box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.2);
+		-webkit-box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.2);
+		-o-box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.2);
+		-ms-box-shadow: 0 1px 5px 0px rgba(0, 0, 0, 0.2);
+		-webkit-transition: all 0.4s;
+		-o-transition: all 0.4s;
+		-moz-transition: all 0.4s;
+		transition: all 0.4s;
+		position: relative;
+		z-index: 1;
+	}
+
+	.btn-face {
+		color: #fff;
+		background-color: #3b5998;
+	}
+
+	.btn-face i {
+		font-size: 30px;
+		margin-right: 17px;
+		padding-bottom: 3px;
+	}
+
+	.btn-google {
+		color: #555555;
+		background-color: #fff;
+	}
+
+	.btn-google img {
+		width: 30px;
+		margin-right: 15px;
+		padding-bottom: 3px;
+	}
+
+	.btn-face:hover:before,
+	.btn-google:hover:before {
+		opacity: 1;
+	}
+
+	.btn-face:hover {
+		color: #fff;
+	}
+
+	.btn-google:hover {
+		color: #39556B;
+	}
+
+	/* Input */
+	.wrap-input100 {
 		width: 100%;
-		height: 100%;
-		padding-left: 35px;
+		position: relative;
+		background-color: #f7f7f7;
+		border: 1px solid #e6e6e6;
+		border-radius: 10px;
+	}
+
+	.input100 {
+		font-family: Poppins-Regular;
+		color: #333333;
+		line-height: 1.2;
+		font-size: 18px;
+
+		display: block;
+		width: 100%;
+		background: transparent;
+		height: 60px;
+		padding: 0 20px;
+	}
+
+	/* Focus Input */
+	.focus-input100 {
+		position: absolute;
+		display: block;
+		width: calc(100% + 2px);
+		height: calc(100% + 2px);
+		top: -1px;
+		left: -1px;
 		pointer-events: none;
-		color: #666666;
+		border: 1px solid #ab8755;
+		border-radius: 10px;
+
+		visibility: hidden;
+		opacity: 0;
 
 		-webkit-transition: all 0.4s;
 		-o-transition: all 0.4s;
 		-moz-transition: all 0.4s;
 		transition: all 0.4s;
+
+		-webkit-transform: scaleX(1.1) scaleY(1.3);
+		-moz-transform: scaleX(1.1) scaleY(1.3);
+		-ms-transform: scaleX(1.1) scaleY(1.3);
+		-o-transform: scaleX(1.1) scaleY(1.3);
+		transform: scaleX(1.1) scaleY(1.3);
 	}
 
-	.input100:focus+.focus-input100+.symbol-input100 {
-		color: #B98F56;
-		padding-left: 28px;
+	.input100:focus+.focus-input100 {
+		visibility: visible;
+		opacity: 1;
+
+		-webkit-transform: scale(1);
+		-moz-transform: scale(1);
+		-ms-transform: scale(1);
+		-o-transform: scale(1);
+		transform: scale(1);
+	}
+
+	.eff-focus-selection {
+		visibility: visible;
+		opacity: 1;
+
+		-webkit-transform: scale(1);
+		-moz-transform: scale(1);
+		-ms-transform: scale(1);
+		-o-transform: scale(1);
+		transform: scale(1);
 	}
 
 	/* Button */
@@ -350,21 +368,9 @@
 		display: -ms-flexbox;
 		display: flex;
 		flex-wrap: wrap;
-		justify-content: center;
-		padding-top: 20px;
 	}
 
 	.login100-form-btn {
-		font-family: Montserrat-Bold;
-		font-size: 15px;
-		line-height: 1.5;
-		color: #fff;
-		text-transform: uppercase;
-
-		width: 100%;
-		height: 50px;
-		border-radius: 25px;
-		background: #B98F56;
 		display: -webkit-box;
 		display: -webkit-flex;
 		display: -moz-box;
@@ -372,54 +378,31 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		padding: 0 25px;
+		padding: 0 20px;
+		width: 100%;
+		height: 60px;
+		background-color: #B98F56;
+		border-radius: 10px;
+
+		font-family: Poppins-Medium;
+		font-size: 16px;
+		color: #fff;
+		line-height: 1.2;
 
 		-webkit-transition: all 0.4s;
 		-o-transition: all 0.4s;
 		-moz-transition: all 0.4s;
 		transition: all 0.4s;
+		position: relative;
+		z-index: 1;
 	}
 
-	.login100-form-btn:hover {
-		background: #333333;
-	}
-
-	/* Responsive */
-	@media (max-width: 992px) {
-		.wrap-login100 {
-			padding: 177px 90px 33px 85px;
-		}
-
-		.login100-pic {
-			width: 35%;
-		}
-
-		.login100-form {
-			width: 50%;
-		}
-	}
-
-	@media (max-width: 768px) {
-		.wrap-login100 {
-			padding: 100px 80px 33px 80px;
-		}
-
-		.login100-pic {
-			display: none;
-		}
-
-		.login100-form {
-			width: 100%;
-		}
-	}
-
-	@media (max-width: 576px) {
-		.wrap-login100 {
-			padding: 100px 15px 33px 15px;
-		}
+	.login100-form-btn:hover:before {
+		opacity: 1;
 	}
 
 	/* Alert validate */
+
 	.validate-input {
 		position: relative;
 	}
@@ -428,22 +411,22 @@
 		content: attr(data-validate);
 		position: absolute;
 		max-width: 70%;
-		background-color: white;
+		background-color: #fff;
 		border: 1px solid #c80000;
-		border-radius: 13px;
-		padding: 4px 25px 4px 10px;
+		border-radius: 2px;
+		padding: 4px 25px 5px 10px;
 		top: 50%;
 		-webkit-transform: translateY(-50%);
 		-moz-transform: translateY(-50%);
 		-ms-transform: translateY(-50%);
 		-o-transform: translateY(-50%);
 		transform: translateY(-50%);
-		right: 8px;
+		right: 12px;
 		pointer-events: none;
 
-		font-family: Poppins-Medium;
+		font-family: Poppins-Regular;
 		color: #c80000;
-		font-size: 13px;
+		font-size: 14px;
 		line-height: 1.4;
 		text-align: left;
 
@@ -457,19 +440,19 @@
 	}
 
 	.alert-validate::after {
-		content: "\f06a";
+		content: "\f12a";
 		font-family: FontAwesome;
 		display: block;
 		position: absolute;
 		color: #c80000;
-		font-size: 15px;
+		font-size: 18px;
 		top: 50%;
 		-webkit-transform: translateY(-50%);
 		-moz-transform: translateY(-50%);
 		-ms-transform: translateY(-50%);
 		-o-transform: translateY(-50%);
 		transform: translateY(-50%);
-		right: 13px;
+		right: 18px;
 	}
 
 	.alert-validate:hover:before {
@@ -483,57 +466,76 @@
 			opacity: 1;
 		}
 	}
+
+	/* Responsive */
+	@media (max-width: 768px) {
+		.wrap-login100 {
+			padding-left: 60px;
+			padding-right: 60px;
+		}
+	}
+
+	@media (max-width: 576px) {
+		.wrap-login100 {
+			padding-left: 15px;
+			padding-right: 15px;
+		}
+	}
 </style>
 
 <body>
 
 	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<div class="login100-pic js-tilt" data-tilt>
-					<img src="" alt="IMG">
-				</div>
-
-				<form class="login100-form validate-form">
-					<span class="login100-form-title">
-						Login - Membro
+		<div class="container-login100" style="background-color: #4c4d4f;">
+			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
+				<form class="login100-form validate-form flex-sb flex-w">
+					<span class="login100-form-title p-b-53">
+						Login Cliente
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
+					<a href="#" class="btn-face m-b-20">
+						<i class="fa fa-facebook-official"></i>
+						Facebook
+					</a>
+
+					<a href="#" class="btn-google">
+						<img src="images/icons/icon-google.png" alt="GOOGLE">
+						Google
+					</a>
+
+					<div class="p-t-31 p-b-9">
+						<span class="txt1">
+							Usuário
 						</span>
 					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+					<div class="wrap-input100 validate-input" data-validate="Required">
+						<input class="input100" type="text" name="username">
 						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-lock" aria-hidden="true"></i>
-						</span>
 					</div>
 
-					<div class="container-login100-form-btn">
+					<div class="p-t-13 p-b-9">
+						<span class="txt1">
+							Senha
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate="Required">
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-17">
 						<button class="login100-form-btn">
-							Login
+							Logar
 						</button>
 					</div>
 
-					<div class="text-center p-t-12">
-						<span class="txt1">
-							Forgot
+					<div class="w-full text-center p-t-55">
+						<span class="txt2">
+							Não é conta?
 						</span>
-						<a class="txt2" href="#">
-							Username / Password?
-						</a>
-					</div>
 
-					<div class="text-center p-t-136">
-						<a class="txt2" href="#">
-							Create your Account
-							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						<a href="#" class="cadastrar">
+							Cadastrar
 						</a>
 					</div>
 				</form>
@@ -541,22 +543,21 @@
 		</div>
 	</div>
 
-
-
+	<div id="dropDownSelect1"></div>
 
 	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
 	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
 	<!--===============================================================================================-->
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
-	<script>
-		$('.js-tilt').tilt({
-			scale: 1.1
-		})
-	</script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
 	<!--===============================================================================================-->
 	<script src="js/main.js"></script>

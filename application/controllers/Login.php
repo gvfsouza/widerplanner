86c2a88a7c7d2833532d75e6b16857eb7c95274d
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-05-03 13:42:38 -0300

    criando login novo

---
+++ application/controllers/Login.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function __construct()
+	{
+	  parent::__construct();
+		
+	  if (isset($_POST)) {
+		if (!empty($_POST)) {
+		  anti_injection($_POST);
+		}
+	  }
+	}
+	
+	public function index()
+	{
+		$this->load->view('login');
+	}
+}

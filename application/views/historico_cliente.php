0956a7722b9504b77d44a6712b1bed13627b55a9
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-05-04 16:38:44 -0300

    .

--- application/views/historico_cliente.php
+++ application/views/historico_cliente.php
@@ -1,5 +1,5 @@
 <ul class="list-group">
-  <li class="list-group-item">An item</li>
+  <li class="list-group-item"><i class="fas fa-cut"></i> An item</li>
   <li class="list-group-item">A second item</li>
   <li class="list-group-item">A third item</li>
   <li class="list-group-item">A fourth item</li>

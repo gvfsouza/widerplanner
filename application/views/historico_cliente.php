3f13663d50002f3c0ff2ea2df8721e7061dad5f5
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-05-06 17:01:13 -0300

    .

--- application/views/historico_cliente.php
+++ application/views/historico_cliente.php
@@ -5,3 +5,69 @@
   <li class="list-group-item">A fourth item</li>
   <li class="list-group-item">And a fifth one</li>
 </ul>
+<section class="container">
+
+	<!-- Header Bar -->
+	<header>
+		<div class="button-col">
+			<button class="btn" name="Add Task"> Add Task </button>
+		</div>
+
+		<div class="status-col">
+			<label> Status </label>
+		</div>
+
+		<div class="progress-col">
+			<label> Progress </label>
+		</div>
+		
+		<div class="dates-col">
+			<label> Dates </label>
+		</div>
+
+		<div class="priority-col">
+			<label> Priority </label>
+		</div>
+		
+		<div class="icon-col">
+			<i class="icon fa fa-user-o"> </i>
+		</div>
+
+	</header>
+
+	<!-- List Items -->
+	<ul class="task-items">
+		
+		<!-- List Item -->
+		<li class="item type1">
+			<div class="task">
+				<div class="icon"> </div>
+				<div class="name" style="width:150px"> </div>
+			</div>
+
+			<div class="status">
+				<div class="icon off"> </div>
+				<div class="text"> Off Track </div>
+			</div>
+
+			<div class="progress">
+				<progress value="15" max="100" />
+			</div>
+
+			<div class="dates">
+				<div class="bar"> </div>
+			</div>
+
+			<div class="priority">
+				<div class="bar"> </div>
+			</div>
+
+			<div class="user">
+				<img src="https://source.unsplash.com/40x40/?indian" alt="Image 001" class="owner-img" />
+			</div>
+		</li>
+		
+		
+	</ul>
+
+</section>

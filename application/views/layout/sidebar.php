654a9864bd4514de69b4c7d5145166cb9150bc96
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-05-06 16:54:42 -0300

    .

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -331,13 +331,13 @@ body.dark .home .text{
                 </li>
                 <ul class="menu-links">
                     <li class="nav-link">
-                        <a href="<?php base_url();?>servicos">
+                        <a href="servicos">
                             <i class='bx bx-home-alt icon'></i>
                             <span class="text nav-text">Serviços</span>
                         </a>
                     </li>
                     <li class="nav-link">
-                        <a href="<?php base_url();?>perfil_cliente">
+                        <a href="perfil_cliente">
                             <i class='bx bx-bar-chart-alt-2 icon'></i>
                             <span class="text nav-text">Revenue</span>
                         </a>

041cd924f7a9b724a4f9e1ac1cf83bfbe8ec7919
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-05-06 16:49:23 -0300

    .

--- application/views/perfil_cliente.php
+++ application/views/perfil_cliente.php
@@ -4,8 +4,8 @@ label{
 }
 </style>
 
-<!-- <div class="card shadow mb-4">
-      <div class="card-header py-3" style="background-color: white;"> -->
+<div class="card shadow mb-4">
+      <div class="card-header py-3" style="background-color: white;">
 
         <div>
           <h3 id="dados-cleinte" style="font-size: 25px;font-weight:bold">Perfil Cliente</h3>
@@ -101,5 +101,5 @@ label{
             </form>
           </div>
         </div>
-      <!-- </div>
-    </div> -->
+      </div>
+    </div>

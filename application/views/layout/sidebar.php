ff1fa61f00d7c7c261344ef58c0e5b86aefe9762
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-05-06 17:25:47 -0300

    .

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -411,29 +411,32 @@ body.dark .home .text {
         </div>
     </nav>
     <section class="home">
-        <div class="text">Dashboard Sidebar</div>
+        <div class="text">
+            Barra Lateral do Painel de Controle
+        </div>
     </section>
-    <script>
-        const body = document.querySelector('body'),
-            sidebar = body.querySelector('nav'),
-            toggle = body.querySelector(".toggle"),
-            searchBtn = body.querySelector(".search-box"),
-            modeSwitch = body.querySelector(".toggle-switch"),
-            modeText = body.querySelector(".mode-text");
+    <script> 
+        const body = document.querySelector('body'); 
+        barra lateral const = body.querySelector('nav'); 
+        const alternar = body.querySelector(".toggle"); 
+        const searchBtn = body.querySelector(".caixa de pesquisa"); 
+        const modeSwitch = body.querySelector(".toggle-switch"); 
+        const modeText = body.querySelector(".mode-text");
         toggle.addEventListener("click", () => {
             sidebar.classList.toggle("close");
-        })
+            });
+
         searchBtn.addEventListener("click", () => {
             sidebar.classList.remove("close");
-        })
+            });
+
         modeSwitch.addEventListener("click", () => {
             body.classList.toggle("dark");
 
             if (body.classList.contains("dark")) {
-                modeText.innerText = "Light mode";
+                modeText.innerText = "Modo Claro";
             } else {
-                modeText.innerText = "Dark mode";
-
+                modeText.innerText = "Modo Escuro";
             }
         });
     </script>

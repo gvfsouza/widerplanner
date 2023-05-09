ad1dd52f4f986a4b1933438ed72b6ed9fc47339c
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-05-09 14:33:11 -0300

    .

--- application/views/agendamento.php
+++ application/views/agendamento.php
@@ -80,10 +80,10 @@
   </div>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="js/bootstrap-datetimepicker.min.js"></script>
-<script src="css/bootstrap-datetimepicker.pt-BR.css"></script>
+<script src="css/bootstrap-datetimepicker.pt-BR.css"></script> -->
 
 <script type="text/javascript">
   $('.data_formato').datetimepicker({

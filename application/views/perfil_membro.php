df175e60be61a2f3b0fc7d56d37aa86a8bc2493f
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-05-15 19:45:59 -0300

    .

---
+++ application/views/perfil_membro.php
@@ -0,0 +1,17 @@
+<style>
+label{
+    color:black;
+}
+</style>
+
+<div class="card shadow mb-4">
+      <div class="card-header py-3" style="background-color: white;">
+
+        <div>
+          <h3 id="dados-cleinte" style="font-size: 25px;font-weight:bold">Perfil Cliente</h3>
+          <br>
+        </div>
+
+        
+      </div>
+    </div>

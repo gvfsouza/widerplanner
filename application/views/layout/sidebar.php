2b51d707083a65f28950bb1bd4c67a718af23d1f
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-07-05 15:42:06 -0300

    .

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -73,4 +73,4 @@
    </ul>
    <!-- End of Sidebar -->
    <!-- Content Wrapper -->
-   <div id="content-wrapper" class="d-flex flex-column">
+   <div id="content-wrapper" class="d-flex flex-column" style="background-color: #C3DFFF">

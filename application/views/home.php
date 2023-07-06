a1ddfb4769a97eba7d80086785fd2650b81dd8c1
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-07-06 10:48:30 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -3,21 +3,26 @@
 		background: #f6f6f6;
 		height: 150px;
 		border-radius: 10px;
-		padding: 1px 48px 48px 450px;
+		padding: 40px;
 		margin: 40px 0;
-		width: 40px;
-		min-width: 180px;
 		display: flex;
-		flex-direction: row;
-		align-self: flex-start;
-		position: relative;
+		align-items: center;
 		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+	}
+
+	section.card .text-content {
+		flex: 1;
+	}
+
+	section.card .text-content h3 {
+		font-size: 22px;
+		margin: 0;
 	}
 
 	section.card .text-content a {
 		border: 3px solid #344966;
 		color: #344966;
-		align-self: flex-start;
+		align-self: flex-end;
 		text-decoration: none;
 		padding: 8px 16px;
 		border-radius: 12px;
@@ -32,10 +37,7 @@
 	section.card .visual {
 		width: 170px;
 		height: 100%;
-		position: absolute;
-		top: -24px;
-		left: 24px;
-		overflow: hidden;
+		margin-left: 24px;
 		border-radius: 24px;
 		box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 	}
@@ -45,22 +47,23 @@
 		width: 100%;
 	}
 </style>
-	
+
 <section class="card">
+	<div class="visual">
+		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
+	</div>
 	<div class="text-content">
-		<h3 style="font-size:22px">Elevate Your Brand with Exceptional Design</h3><br><br>
+		<h3>Elevate Your Brand with Exceptional Design</h3>
 		<a href="perfil_estabelecimento">Agendar</a>
 	</div>
-	<div class="visual">
-		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
-	</div>
 </section>
+
 <section class="card">
-	<div class="text-content">
-		<h3 style="font-size:22px">Elevate Your Brand with Exceptional Design</h3><br><br>
-		<a href="#">Agendar</a>
-	</div>
 	<div class="visual">
 		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
+	</div>
+	<div class="text-content">
+		<h3>Elevate Your Brand with Exceptional Design</h3>
+		<a href="#">Agendar</a>
 	</div>
 </section>

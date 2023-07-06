584d62fb3c3d3d7c8ab7bfd777db0fb8f55ff7aa
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-07-06 11:22:00 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -1,67 +1,70 @@
 <style>
-	section.card {
-		background: #f6f6f6;
-		height: 150px;
-		border-radius: 10px;
-		padding: 1px 48px 48px 450px;
-		margin: 40px 0;
-		width: 40px;
-		min-width: 180px;
-		display: flex;
-		flex-direction: row;
-		align-self: flex-start;
-		position: relative;
-		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-	}
+    section.card {
+        background: #f6f6f6;
+        border-radius: 10px;
+        padding: 40px;
+        margin: 40px 0;
+        display: flex;
+        align-items: center;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
 
-	section.card .text-content a {
-		border: 3px solid #344966;
-		color: #344966;
-		align-self: flex-start;
-		text-decoration: none;
-		padding: 8px 16px;
-		border-radius: 12px;
-		transition: 400ms all;
-	}
+    section.card .text-content {
+        flex: 1;
+    }
 
-	section.card .text-content a:hover {
-		background: #344966;
-		color: white;
-	}
+    section.card .text-content h3 {
+        font-size: 22px;
+        margin: 0;
+    }
 
-	section.card .visual {
-		width: 170px;
-		height: 100%;
-		
-		position: absolute;
-		top: -24px;
-		left: 24px;
-		overflow: hidden;
-		border-radius: 24px;
-		box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-	}
+    section.card .text-content a {
+        border: 3px solid #344966;
+        color: #344966;
+        text-decoration: none;
+        padding: 8px 16px;
+        border-radius: 12px;
+        transition: 400ms all;
+    }
 
-	section.card .visual img {
-		height: 100%;
-		width: 100%;
-	}
+    section.card .text-content a:hover {
+        background: #344966;
+        color: white;
+    }
+
+    section.card .visual {
+        width: 170px;
+        height: 100%;
+        margin-right: 24px;
+        border-radius: 24px;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+    }
+
+    section.card .visual img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
 </style>
-	
+
 <section class="card">
-	<div class="text-content">
-		<h3 style="font-size:22px">Elevate Your Brand with Exceptional Design</h3><br><br>
-		<a href="perfil_estabelecimento">Agendar</a>
-	</div>
-	<div class="visual">
-		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
-	</div>
+    <div class="visual">
+        <img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
+    </div>
+    <div class="text-content">
+        <h3>Elevate Your Brand with Exceptional Design</h3>
+        <br><br>
+        <a href="perfil_estabelecimento">Agendar</a>
+    </div>
 </section>
+
 <section class="card">
-	<div class="text-content">
-		<h3 style="font-size:22px">Elevate Your Brand with Exceptional Design</h3><br><br>
-		<a href="#">Agendar</a>
-	</div>
-	<div class="visual">
-		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
-	</div>
+    <div class="visual">
+        <img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
+    </div>
+    <div class="text-content">
+        <h3>Elevate Your Brand with Exceptional Design</h3>
+        <br><br>
+        <a href="#">Agendar</a>
+    </div>
 </section>

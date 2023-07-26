e46054783815487611cb8388c7ba9f5f140a10b9
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-07-26 16:49:41 -0300

    ..

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://widerplanner.agsete.com.br//';
+$config['base_url'] = 'https://widerplanner.agsete.com.br/';
 
 /*
 |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'portuguese-brazilian';
+$config['language']	= 'english';
 
 /*
 |--------------------------------------------------------------------------

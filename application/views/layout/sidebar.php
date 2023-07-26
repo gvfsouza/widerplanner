353f3d320b1bd7bf2429fc8b09a812176b35066e
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-07-26 16:52:50 -0300

    .

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -55,7 +55,7 @@
        </li>
 
        <li class="nav-item">
-           <a title="Gerenciar Usuários" class="nav-link" href="#">
+           <a title="Gerenciar Usuários" class="nav-link" href="<?php echo base_url(); ?>perfil_estabelecimento">
             <i class="fas fa-user icon"></i>
                <span class="link">Perfil </span>
            </a>

f92f320c4080fe92693337a3e5b36a22d56b815e
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-07-26 17:41:10 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -5,6 +5,8 @@
 		border-radius: 10px;
 		margin: 40px 0;
 		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+		position: relative;
+		overflow: hidden;
 	}
 
 	section.card .text-content {
@@ -20,10 +22,10 @@
 		border: 3px solid #344966;
 		color: #344966;
 		text-decoration: none;
-		padding: 8px 16px;
-		border-radius: 12px;
+		padding: 6px 12px;
+		border-radius: 8px;
 		transition: 400ms all;
-		margin-top: 20px;
+		margin-top: 10px;
 	}
 
 	section.card .text-content a:hover {
@@ -44,11 +46,11 @@
 		}
 
 		section.card .visual {
-			width: 170px;
+			width: 200px;
 			height: 100%;
-			position: relative;
-			top: 0;
-			left: 0;
+			position: absolute;
+			top: -24px;
+			left: -30px; /* Adjust the left positioning as needed */
 			overflow: hidden;
 			border-radius: 24px;
 			box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
@@ -62,20 +64,20 @@
 </style>
 
 <section class="card">
-	<div class="text-content">
-		<h3>Elevate Your Brand with Exceptional Design</h3>
-		<a href="perfil_estabelecimento">Visualizar Estabelecimento</a>
-	</div>
 	<div class="visual">
 		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
+	</div>
+	<div class="text-content">
+		<h3>Elevate Your Brand with Exceptional Design</h3>
+		<a href="perfil_estabelecimento">Ver Estabelecimento</a>
 	</div>
 </section>
 <section class="card">
-	<div class="text-content">
-		<h3>Elevate Your Brand with Exceptional Design</h3>
-		<a href="#">Visualizar Estabelecimento</a>
-	</div>
 	<div class="visual">
 		<img src="https://images.unsplash.com/photo-1548142813-c348350df52b?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzgzMDU1NDY&ixlib=rb-4.0.3&q=80" alt="" />
+	</div>
+	<div class="text-content">
+		<h3>Elevate Your Brand with Exceptional Design</h3>
+		<a href="#">Ver Estabelecimento</a>
 	</div>
 </section>

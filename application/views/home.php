239b2732a560c5eb74a28da8a32a0189f8410289
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-07-26 18:01:24 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -7,6 +7,7 @@
 		box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 		display: flex;
 		flex-direction: column;
+		position: relative; /* Add a relative position to the card */
 	}
 
 	section.card .text-content {
@@ -45,9 +46,9 @@
 		section.card .visual {
 			width: 150px; /* Adjust the width of the image for larger screens */
 			height: 150px; /* Adjust the height of the image for larger screens */
-			margin-right: 20px; /* Added some spacing between the image and text */
 			position: absolute;
-			top: -30px; /* Position the image above the card */
+			top: -75px; /* Position the image above the card */
+			left: 20px; /* Add some left spacing for better alignment */
 		}
 
 		section.card .visual img {

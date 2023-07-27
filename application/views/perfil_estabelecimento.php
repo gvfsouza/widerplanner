d37897b39a7dca3b447b26c697bed508d1582bb9
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-07-27 08:47:34 -0300

    .

--- application/views/perfil_estabelecimento.php
+++ application/views/perfil_estabelecimento.php
@@ -31,13 +31,36 @@
                         </legend>
                       </div>
                     </fieldset>
-                    <fieldset class="form-group"
-                      style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                    <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
                       <div class="row">
                         <div style="margin-left:10px;"></div>
-
                         <div class="col">
-                          
+                          <div class="row">
+                              <div class="form-group col-md-6">
+                                <label for="" style="color: #77787a;"></label>
+                                <br>
+                                <input style="font-size:14px" type="text" class="form-control" id="" name="" value="">
+                              </div>
+                              <div class="dados-cliente form-group col-md-6">
+                                <label for="" style="color: #77787a;">:</label>
+                                <br>
+                                <input type="text" class="form-control" id="" name="">
+                              </div>
+                              <div class="dados-cliente form-group col-md-4">
+                                <label for="" style="color: #77787a;">:</label>
+                                <br>
+                                <input type="" class="form-control" id="" name="" value="">
+                              </div>
+                              <div class="dados-cliente form-group col-md-2">
+                                <label for="" style="color: #77787a;">:</label>
+                                <br>
+                                <input style="font-size:14px" type="text" class="form-control" id="" name="" value="">
+                              </div>
+                              <div class="dados-cliente col-md-6">
+                                <label for="" style="color: #77787a;">:</label>
+                                <input style="font-size:14px" type="text" class="form-control" id="" name="" value="">
+                              </div>
+                          </div>
                         </div>
                       </div>
                     </fieldset>

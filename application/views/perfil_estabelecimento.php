45bf2359a02bf5635b91f95fcd247d450c5b9a17
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-08-01 10:11:18 -0300

    .

--- application/views/perfil_estabelecimento.php
+++ application/views/perfil_estabelecimento.php
@@ -12,7 +12,7 @@
             padding: 2rem;
             background-color: white;
             border-radius: 1rem;
-            box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.6);
+            box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
             margin: 12px;
             margin-top: 40px;
         }

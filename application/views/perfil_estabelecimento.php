98e86c5db73cc3262ed3dd0a59a7de55ea6793c4
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-08-01 10:52:37 -0300

    ;

--- application/views/perfil_estabelecimento.php
+++ application/views/perfil_estabelecimento.php
@@ -26,6 +26,7 @@
             height: 6.5rem;
             background-size: cover;
             position: center;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
         }
 
         .profile-info {

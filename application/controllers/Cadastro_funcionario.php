28bd6b04d4abbde69cc9af1d86f4a200455a9ac3
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-09-24 17:08:46 -0300

    Cadastro funcionário

---
+++ application/controllers/Cadastro_funcionario.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cadastro_funcionario extends CI_Controller {
+	public function __construct()
+	{
+	  parent::__construct();
+		
+	  if (isset($_POST)) {
+		if (!empty($_POST)) {
+		  anti_injection($_POST);
+		}
+	  }
+	}
+	
+	public function index()
+	{
+		$this->load->view('layout/header');
+		$this->load->view('layout/sidebar');
+		// $this->load->view('layout/navbar');
+		$this->load->view('cadastro_funcionario');
+		$this->load->view('layout/footer');
+	}
+}
---
+++ application/views/cadastro_funcionario.php
@@ -0,0 +1,75 @@
+<style>
+label{
+    color:black;
+}
+</style>
+
+<br>
+
+<div>
+    <div class="container-fluid">
+      <div class="card shadow mb-4">
+            <div class="card-header py-3" style="background-color: white;">
+
+              <div>
+                <h3 id="dados-cleinte" style="font-size: 25px;font-weight:bold">Cadastro Profissional</h3>
+                <br>
+              </div>
+
+              <!-- FORMULÁRIO  -->
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="" role="tabpanel">
+                  <form action="">
+                    <div class="p-3 bg-white">
+                      <div class="row">
+                        <div class="col-12">
+                          <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                            <div class="row">
+                              <div style="margin-left:10px;"></div>
+                              <div class="col">
+                                <div class="row">
+                                  <div class="form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Foto:</label>
+                                    <br>
+                                    <input style="font-size:14px" type="file" class="form-control" id="foto" name="foto" value="">
+                                  </div>
+                                  <div class="form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Nome Completo do Funcionário:</label>
+                                    <br>
+                                    <input style="font-size:14px" type="text" class="form-control" id="nome_funcionario" name="nome_funcionario" value="">
+                                  </div>
+                                  <div class="form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Idade:</label>
+                                    <br>
+                                    <input style="font-size:14px" type="text" class="form-control" id="nome_funcionario" name="nome_funcionario" value="">
+                                  </div>
+                                  <div class="dados-cliente form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Profissão:</label>
+                                    <br>
+                                    <textarea class="form-control" name="profissao" id="profissao" cols="5" rows="3"></textarea>
+                                  </div>
+                                  <div class="dados-cliente form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Quanto tempo está na área :</label>
+                                    <br>
+                                    <input type="number" class="form-control" id="tempo_area" name="tempo_area" value="">
+                                  </div>
+                                  <div class="dados-cliente form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Serviços:</label>
+                                    <br>
+                                    <input style="font-size:14px" type="time" class="form-control" id="servicos" name="servicos" value="">
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </fieldset>
+                          <br>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>

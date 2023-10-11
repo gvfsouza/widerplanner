4e3d5517f73afee15f062079806e6b41c9e1b593
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-11 16:06:46 -0300

    .

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -8,56 +8,44 @@
     <meta name="author" content="">
     <link href="public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="public/css/app.css">
-    <link rel="stylesheet" href="public/css/bootstrap-multiselect.css">
-    <link rel="stylesheet" href="public/css/bootstrap-multiselect.min.css">
-    <link rel="stylesheet" href="public/css/sb-admin-2.css">
-    <link rel="stylesheet" href="public/css/sb-admin-2.min.css">
-    <link rel="stylesheet" href="public/css/bootstrap-select.min.css">
-    <script src="public/js/mascaras.js"></script>
-    <script src="public/js/funcoes.js"></script>
-    <script type="text/javascript" src="public/js/bootstrap-select.min.js"></script>
-    <script src="public/js/apexcharts.js"></script>
-    <script src="public/vendor/jquery/jquery.min.js"></script>
-    <script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="public/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="public/js/sb-admin-2.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-	<link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/app.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/bootstrap-multiselect.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/bootstrap-multiselect.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/sb-admin-2.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/sb-admin-2.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/bootstrap-select.min.css') ?>">
+    <script src="<?php echo base_url('public/js/funcoes.js') ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('public/js/bootstrap-select.min.js') ?>"></script>
+    <script src="<?php echo base_url('public/js/apexcharts.js') ?>"></script>
+    <script src="<?php echo base_url('public/vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?php echo base_url('public/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?php echo base_url('public/js/sb-admin-2.min.js') ?>"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/select2/select2.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('fonts/font-awesome-4.7.0/css/font-awesome.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/animate/animate.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/css-hamburgers/hamburgers.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/animsition/css/animsition.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('vendor/daterangepicker/daterangepicker.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/util.css') ?>">
     <title>WiderPlanner </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
     <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="css/bootstrap-multiselect.css">
-    <link rel="stylesheet" href="css/bootstrap-multiselect.min.css">
-    <link rel="stylesheet" href="css/sb-admin-2.css">
-    <link rel="stylesheet" href="css/sb-admin-2.min.css">
-    <link rel="stylesheet" href="css/bootstrap-select.min.css">
-    <script src="jquery/jquery.min.js"></script>
-    <script src="jquery/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-    <script src="js/mascaras.js"></script>
-    <script src="js/funcoes.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url('css/app.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/bootstrap-multiselect.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/bootstrap-multiselect.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/sb-admin-2.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/sb-admin-2.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/bootstrap-select.min.css') ?>">
+    <script src="<?php echo base_url('bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?php echo base_url('js/sb-admin-2.min.js') ?>"></script>
+    <script src="<?php echo base_url('js/funcoes.js') ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('js/bootstrap-select.min.js') ?>"></script>
 </head>
+
 <body id="page-top">
     <div id="wrapper" class="h-100" style="background-color: ##d9e2f7;">
 

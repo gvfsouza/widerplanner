78b48576b0988b21badc40867a8ce28db87ca279
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-11 13:17:08 -0300

    .

--- application/views/agendamento.php
+++ application/views/agendamento.php
@@ -1,7 +1,42 @@
+<style>
+
+    .container-agendamento {
+        width: 100%;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .wrap-agendamento {
+        width: 465px;
+        background: #fff;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .form-agendamento {
+        width: 100%;
+    }
+
+    .form-agendamento-title {
+        width: 100%;
+        display: block;
+        font-family: Montserrat-Medium;
+        font-size: 30px; /* Tamanho maior */
+        color: #555555;
+        line-height: 1.2;
+        text-align: center;
+        margin-bottom: 20px;
+        font-weight: bold; /* Em negrito */
+    }
+</style>
+
 <div class="container-agendamento">
     <div class="wrap-agendamento">
         <form class="form-agendamento">
-
+           
         </form>
     </div>
 </div>

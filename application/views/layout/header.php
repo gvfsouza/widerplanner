bfc37be74c9dde69b78b70b22ab15af1ab68d162
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-11 15:37:08 -0300

    .

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -3,69 +3,29 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="public/css/app.css">
-    <link rel="stylesheet" href="public/css/bootstrap-multiselect.css">
-    <link rel="stylesheet" href="public/css/bootstrap-multiselect.min.css">
-    <link rel="stylesheet" href="public/css/sb-admin-2.css">
-    <link rel="stylesheet" href="public/css/sb-admin-2.min.css">
-    <link rel="stylesheet" href="public/css/bootstrap-select.min.css">
-    <script src="public/js/mascaras.js"></script>
-    <script src="public/js/funcoes.js"></script>
-    <script type="text/javascript" src="public/js/bootstrap-select.min.js"></script>
-    <script src="public/js/apexcharts.js"></script>
-    <!-- ARQUIVOS DO FOOTER -->
-    <script src="public/vendor/jquery/jquery.min.js"></script>
-    <script src="public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="public/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="public/js/sb-admin-2.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-    <title>WiderPlanner </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap">
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- Custom fonts for this template-->
-    <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="icon" href="https://www.indaiatuba.sp.gov.br/favicon.ico" type="image/x-icon">
+    <link href="<?php echo base_url('public/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <!-- Custom styles for this template-->
-    <!-- Importanto CSS do template -->
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="css/bootstrap-multiselect.css">
-    <link rel="stylesheet" href="css/bootstrap-multiselect.min.css">
-    <link rel="stylesheet" href="css/sb-admin-2.css">
-    <link rel="stylesheet" href="css/sb-admin-2.min.css">
-    <!-- Select com opções e filtro -->
-    <link rel="stylesheet" href="css/bootstrap-select.min.css">
-    <script src="jquery/jquery.min.js"></script>
-    <!-- Bootstrap core JavaScript-->
-    <script src="jquery/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Core plugin JavaScript-->
-    <script src="jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-    <script src="js/mascaras.js"></script>
-    <script src="js/funcoes.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url('public/css/app.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/sb-admin-2.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('public/css/sb-admin-2.css') ?>">
+
+    <script src="<?php echo base_url('public/js/mascaras.js'); ?>"></script>
+    <script src="<?php echo base_url('public/js/funcoes.js'); ?>"></script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Select com opções e filtro -->
-    <script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+
+    <script src="https://minha.indaiatuba.sp.gov.br/js/jquery.js"></script>
+    <script type="text/javascript" src="https://minha.indaiatuba.sp.gov.br/js/popper.min.js"></script>
+    <script src="<?php echo base_url('public/js/sb-admin-2.min.js'); ?>"></script>
+    <link rel="stylesheet" href="<?php echo base_url('public/css/select2.min.css') ?>">
+    <script src="<?php echo base_url('public/js/select2.min.js'); ?>"></script>
 </head>
+
 <body id="page-top">
     <div id="wrapper" class="h-100" style="background-color: #d9e2f7;">
-
---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.0',
+      'version' => '5.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb0e820b8436873675fd3aca04f3728eb2185cb',
+    ),
+  ),
+);

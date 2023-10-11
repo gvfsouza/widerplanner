913201cd6b2c21d3780759e97ec648c6f6051824
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-11 16:08:30 -0300

    .

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -35,11 +35,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap">
-    <meta charset="utf-8">
+    <!-- <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content=""> -->
     <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="css/app.css">
@@ -48,7 +48,6 @@
     <link rel="stylesheet" href="css/sb-admin-2.css">
     <link rel="stylesheet" href="css/sb-admin-2.min.css">
     <link rel="stylesheet" href="css/bootstrap-select.min.css">
-    <script src="jquery/jquery.min.js"></script>
     <script src="jquery/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="jquery-easing/jquery.easing.min.js"></script>

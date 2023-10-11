eecb4ed84d98968f0d1fb2fa671f318980e9e490
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-11 12:45:05 -0300

    .

--- application/controllers/Cadastro_cliente.php
+++ application/controllers/Cadastro_cliente.php
@@ -15,7 +15,7 @@ class Cadastro_cliente extends CI_Controller {
 	
 	public function index()
 	{
-		// $this->load->view('layout/header');
+		$this->load->view('layout/header');
 		// $this->load->view('layout/sidebar');
 		// $this->load->view('layout/navbar');
 		$this->load->view('cadastro_cliente');

ad3b88be8863aa50f94da0fc68b56f6e064072fc
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-11 12:32:21 -0300

    .

--- application/views/login.php
+++ application/views/login.php
@@ -1,80 +1,79 @@
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@100;300;400;500;700;900&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap');
+  @import url("https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@100;300;400;500;700;900&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap");
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'poppins', sans-serif;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "poppins", sans-serif;
+  }
 
-body {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #879bc942
-}
+  body {
 
-.container {
-  background-color: white;
-  padding: 6px;
-  width: 350px;
-  height: 450px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-  border-radius: 10px;
-  box-shadow: 0 0 20px 0 darkgrey;
-}
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #879bc942;
+  }
 
-form {
-  position: absolute;
-  margin-top: 80px;
-  transition: .5s;
-  width: 300px;
-  height: 400px;
-}
+  .container {
+    background-color: white;
+    padding: 6px;
+    width: 350px;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 0 0 20px 0 darkgrey;
+  }
 
-.buttonsForm {
-  margin-top: 40px;
-  position: relative;
-  border-radius: 5px;
-  box-shadow: 0 0 7px 0 #879bc9;
-}
+  form {
+    position: absolute;
+    margin-top: 80px;
+    transition: 0.5s;
+    width: 300px;
+    height: 400px;
+  }
 
-.buttonsForm button {
-  cursor: pointer;
-  background: transparent;
-  border: none;
-  position: relative;
-  padding: 10px 29px;
-}
+  .buttonsForm {
+    margin-top: 40px;
+    position: relative;
+    border-radius: 5px;
+    box-shadow: 0 0 7px 0 #879bc9;
+  }
 
-.btnColor {
-  position: absolute;
-  width: 108px;
-  height: 100%;
-  background: #879bc9;
-  border-radius: 5px;
-  transition: .3s;
-}
+  .buttonsForm button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    position: relative;
+    padding: 10px 29px;
+  }
 
-input[type="text"], input[type="password"] {
-  border: none;
-  margin-top: 40px;
-  border-radius: 5px;
-  padding: 8px 0 8px 35px;
-  outline: none;
-  width: 100%;
-  box-shadow: 0 0 7px 0 #879bc9;
-}
+  .btnColor {
+    position: absolute;
+    width: 108px;
+    height: 100%;
+    background: #879bc9;
+    border-radius: 5px;
+    transition: 0.3s;
+  }
 
+  input[type="text"],
+  input[type="password"] {
+    border: none;
+    margin-top: 10px;
+    border-radius: 5px;
+    padding: 8px 0 8px 35px;
+    outline: none;
+    width: 100%;
+    box-shadow: 0 0 7px 0 #879bc994;
+  }
 
-button[type="submit"] {
+  button[type="submit"] {
     background: #879bc9;
     color: white;
     border-radius: 5px;
@@ -85,94 +84,69 @@ button[type="submit"] {
     font-size: 15px;
     margin-top: 40px;
     cursor: pointer;
-}
+  }
 
-.divCheck {
-  margin: 10px 0 0 5px;
-  width: 80%;
-  font-size: 13px;
-}
+  .divCheck {
+    margin: 10px 0 0 5px;
+    width: 80%;
+    font-size: 13px;
+  }
 
-#signin {
-  left: 25px;
-}
+  #signin {
+    left: 25px;
+  }
 
-#signup {
-  left: 450px;
-}
+  #signup {
+    left: 450px;
+  }
 
-i {
-  position: absolute;
-  left: 0;
-  padding: 10px 10px;
-}
+  i {
+    position: absolute;
+    left: 0;
+    padding: 10px 10px;
+  }
 
-.iEmail {
-  top: 40px;
-}
+  .iPassword {
+    top: 114px;
+  }
 
-.iPassword {
-  top: 114px;
-}
+  .iPassword2 {
+    top: 190px;
+  }
 
-.iPassword2 {
-  top: 190px;
-}
+  label{
+    color: #879BC9;
+  }
+
+  a{
+    text-decoration: none;
+  }
 </style>
 
-  <div class="container">
-    <div class="buttonsForm">
-      <div class="btnColor"></div>
-      <button id="btnSignin" style="border-radius:5px">Sign in</button>
-      <button id="btnSignup">Sign up</button>
+<div class="container">
+
+
+  <form id="signin">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label for="cpf"> CPF:</label>
+                <input type="text" class="form-control" placeholder="Digite o CPF" name="cpf" id="cpf">
+            </div>
+        </div>
+        <br>
+        <div class="col-md-12">
+            <label for="senha"> Senha:</label>
+            <input type="password" class="form-control" placeholder="Insira sua senha" name="senha" id="senha">
+        </div>
     </div>
-
-    <form id="signin">
-      <input type="text" placeholder="Email" required />
-      <i class="fas fa-envelope iEmail"></i>
-      <input type="password" placeholder="Password" required />
-      <i class="fas fa-lock iPassword"></i>
-      <div class="divCheck">
-        <input type="checkbox" />
-        <span>Remember Password</span>
-      </div>
-      <button type="submit">Sign in</button>
-    </form>
-
-    <form id="signup">
-      <input type="text" placeholder="Email" required />
-      <i class="fas fa-envelope iEmail"></i>
-      <input type="password" placeholder="Password" required />
-      <i class="fas fa-lock iPassword"></i>
-      <input type="password" placeholder="Password" required />
-      <i class="fas fa-lock iPassword2"></i>
-      <div class="divCheck">
-        <input type="checkbox" required />
-        <span>Terms</span>
-      </div>
-      <button type="submit">Sign up</button>
-    </form>
-  </div>
-
-  <script> 
-  var formSignin = document.querySelector('#signin')
-var formSignup = document.querySelector('#signup')
-var btnColor = document.querySelector('.btnColor')
-
-document.querySelector('#btnSignin')
-  .addEventListener('click', () => {
-    formSignin.style.left = "25px"
-    formSignup.style.left = "450px"
-    btnColor.style.left = "0px"
-})
-
-document.querySelector('#btnSignup')
-  .addEventListener('click', () => {
-    formSignin.style.left = "-450px"
-    formSignup.style.left = "25px"
-    btnColor.style.left = "110px"
-})
-  
-</script>
-
-
+    <div class="divCheck">
+      <br>
+      <span><a href="">Esqueci Senha</a></span>
+      <br>
+      <br>
+      <span><a href=""><strong>Clique aqui</strong> para realizar o cadastro</a></span>
+    </div>
+    <button type="submit">Entrar</button>
+  </form>
+</div>

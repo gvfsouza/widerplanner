5298911dc5002ab3d9da3c2c22a93bb069de0ed9
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-11 13:15:27 -0300

    .

--- application/controllers/Agendamento.php
+++ application/controllers/Agendamento.php
@@ -16,6 +16,7 @@ class Agendamento extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('layout/header');
+		$this->load->view('layout/sidebar');
 		$this->load->view('agendamento');
     }
 }

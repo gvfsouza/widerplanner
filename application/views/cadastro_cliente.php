b60ebd36c5af7f204e04296f65be2c25c6a8e417
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-11 12:50:36 -0300

    .

--- application/views/cadastro_cliente.php
+++ application/views/cadastro_cliente.php
@@ -1,23 +1,122 @@
 <style>
-label{
-    color:black;
-}
+  @import url("https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@100;300;400;500;700;900&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap");
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "poppins", sans-serif;
+  }
+
+  body {
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #879bc942;
+  }
+
+  .container {
+    background-color: white;
+    padding: 6px;
+    width: 350px;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 0 0 20px 0 darkgrey;
+  }
+
+  form {
+    position: absolute;
+    margin-top: 80px;
+    transition: 0.5s;
+    width: 300px;
+    height: 400px;
+  }
+
+  .buttonsForm {
+    margin-top: 40px;
+    position: relative;
+    border-radius: 5px;
+    box-shadow: 0 0 7px 0 #879bc9;
+  }
+
+  .buttonsForm button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    position: relative;
+    padding: 10px 29px;
+  }
+
+  .btnColor {
+    position: absolute;
+    width: 108px;
+    height: 100%;
+    background: #879bc9;
+    border-radius: 5px;
+    transition: 0.3s;
+  }
+
+  input[type="text"],
+  input[type="password"] {
+    border: none;
+    margin-top: 10px;
+    border-radius: 5px;
+    padding: 8px 0 8px 35px;
+    outline: none;
+    width: 100%;
+    box-shadow: 0 0 7px 0 #879bc994;
+  }
+
+  button[type="submit"] {
+    background: #879bc9;
+    color: white;
+    border-radius: 5px;
+    width: 100%;
+    border: none;
+    outline: none;
+    padding: 8px 0 8px 15px;
+    font-size: 15px;
+    margin-top: 40px;
+    cursor: pointer;
+  }
+
+  .divCheck {
+    margin: 10px 0 0 5px;
+    width: 80%;
+    font-size: 13px;
+  }
+
+  i {
+    position: absolute;
+    left: 0;
+    padding: 10px 10px;
+  }
+
+  .iPassword {
+    top: 114px;
+  }
+
+  .iPassword2 {
+    top: 190px;
+  }
+
+  label{
+    color: #879BC9;
+  }
+
+  a{
+    text-decoration: none;
+  }
 </style>
 
-<br>
-
-<div>
-    <div class="container-fluid">
-      <div class="card shadow mb-4">
-            <div class="card-header py-3" style="background-color: white;">
-
-              <div>
-                <h3 id="dados-cleinte" style="font-size: 25px;font-weight:bold">Cadastro de Cliente</h3>
-                <br>
-              </div>
-
-             <!-- FORMULÁRIO  -->
-             <div class="tab-content" id="myTabContent">
+<div class="container">
+<div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="" role="tabpanel">
                   <form action="">
                     <div class="p-3 bg-white">
@@ -101,5 +200,4 @@ label{
                 </div>
               </div>
           </div>
-        </div>
-      </div>
+</div>

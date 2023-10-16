39c2e7b3a786246002c48b228911e41bea498e9c
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-16 10:41:18 -0300

    .

--- application/views/lista_agendamentos.php
+++ application/views/lista_agendamentos.php
@@ -1,34 +1,225 @@
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Quem Agendou</th>
-      <th scope="col">Data</th>
-      <th scope="col">Hora</th>
-      <th scope="col">Serviço</th>
-      <th scope="col">Profissional</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+
+<style>
+    label {
+        color: black;
+    }
+
+    .card-profile {
+        width: 350px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 2rem;
+        background-color: white;
+        border-radius: 1rem;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+        margin: 12px;
+        margin-top: 40px;
+    }
+
+    .profile-image {
+        position: relative;
+        top: -70px;
+        border-radius: 50%;
+        background-image: url(https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80);
+        width: 6.5rem;
+        height: 6.5rem;
+        background-size: cover;
+        position: center;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+    }
+
+    .profile-info {
+        text-align: center;
+        /* margin-top: -3rem; */
+        margin-bottom: 1rem;
+    }
+
+
+    .profile-name {
+        border-radius: 5px;
+        font-size: 18px;
+        color: black;
+    }
+
+    .profile-info>.profile-desc {
+        color: #666666;
+        font-size: 0.9rem;
+    }
+
+    .status-value {
+        display: flex;
+        align-items: center;
+    }
+
+    .status-value i {
+        margin-left: 10px;
+    }
+
+    .action {
+        display: flex;
+    }
+
+    .btn {
+        border: none;
+        padding: 0.8em 1.9em;
+        border-radius: 0.35rem;
+        cursor: pointer;
+        font-weight: 600;
+    }
+
+    .btn-success {
+        background: #879bc9;
+        color: white;
+    }
+
+    .btn-outline {
+        border: 1px solid;
+        background: transparent;
+        color: #133185;
+    }
+
+    .btn-success:hover {
+        background: #1331859d;
+        color: white;
+    }
+
+    .btn-outline:hover {
+        border: 1px solid;
+        background: transparent;
+        color: #1331859d;
+    }
+
+    .card-container {
+        display: flex;
+        overflow-x: auto;
+        /* white-space: nowrap; */
+    }
+
+    .flex-container {
+        display: flex;
+        column-gap: 15px;
+        max-width: 1200px;
+        width: 100%;
+        margin: 0 auto;
+        /* margin-top: 60px; */
+    }
+
+    .space-between {
+        justify-content: space-between;
+    }
+
+    .col {
+        width: 50%;
+    }
+
+    .card {
+        border: 1px solid #eee;
+        border-radius: 15px;
+        padding: 20px;
+        background-color: #fff;
+        display: flex;
+        column-gap: 20px;
+        box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+        -webkit-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+        -moz-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+    }
+
+    .card img {
+        max-width: 250px;
+        transform: translateY(-15%);
+    }
+
+    .img-placeholder {
+        position: relative;
+        max-height: 150px;
+        overflow: hidden;
+        border-radius: 15px;
+        box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+        -webkit-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+        -moz-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+    }
+
+    h3 {
+        font-weight: 400;
+    }
+
+    p {
+        font-weight: 300;
+    }
+
+    a {
+        color: #888;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: inherit;
+    }
+</style>
+
+<br>
+
+<div>
+    <div class="container-fluid">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3" style="background-color: white;">
+
+                <div>
+                    <h3 id="dados-cleinte" style="font-size: 25px;font-weight:bold"></h3>
+                    <br>
+                </div>
+
+                <!-- FORMULÁRIO  -->
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="" role="tabpanel">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">Quem Agendou</th>
+                                <th scope="col">Data</th>
+                                <th scope="col">Hora</th>
+                                <th scope="col">Serviço</th>
+                                <th scope="col">Profissional</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                    <span class="text-center" style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
+                        Projeto de Conclusão de Curso - Superior de
+                        Tecnologia em Análise e Deselvolvimento de Sistemas <br> Centro Universitário Nossa Senhora do
+                        Patrocínio <br> Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+    <a class="btn" href="#" role="button"
+        style="float: right; background-color: #1cc88a; color: white; margin-bottom: 15px; position: fixed;">Agenda
+    </a>
+</div>

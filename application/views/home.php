c908124f6dfa4a648e1a453944bf0c6a18a55bbe
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-16 08:46:07 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -94,6 +94,67 @@
         overflow-x: auto;
         /* white-space: nowrap; */
     }
+
+    .flex-container {
+        display: flex;
+        column-gap: 15px;
+        max-width: 1200px;
+        width: 100%;
+        margin: 0 auto;
+        margin-top: 60px;
+    }
+
+    .space-between {
+        justify-content: space-between;
+    }
+
+    .col {
+        width: 50%;
+    }
+
+    .card {
+        border: 1px solid #eee;
+        border-radius: 15px;
+        padding: 20px;
+        background-color: #fff;
+        display: flex;
+        column-gap: 20px;
+        box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+        -webkit-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+        -moz-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.5);
+    }
+
+    .card img {
+        max-width: 250px;
+        transform: translateY(-15%);
+    }
+
+    .img-placeholder {
+        position: relative;
+        max-height: 200px;
+        overflow: hidden;
+        border-radius: 15px;
+        box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+        -webkit-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+        -moz-box-shadow: 1px 1px 16px -6px rgba(0, 0, 0, 0.75);
+    }
+
+    h3 {
+        font-weight: 400;
+    }
+
+    p {
+        font-weight: 300;
+    }
+
+    a {
+        color: #888;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: inherit;
+    }
 </style>
 
 <br>
@@ -134,7 +195,8 @@
                                                             <div class="profile-image"></div>
                                                         </center>
                                                         <div class="profile-info">
-                                                            <h3 class="profile-name"><strong>Corte de Cabelo</strong></h3>
+                                                            <h3 class="profile-name"><strong>Corte de Cabelo</strong>
+                                                            </h3>
                                                             <br>
                                                             <p class="profile-desc">Descrição (se tiver)</p>
                                                         </div>
@@ -185,7 +247,8 @@
                                                             <div class="profile-image"></div>
                                                         </center>
                                                         <div class="profile-info">
-                                                            <h3 class="profile-name"><strong>Pigmentação  em barba</strong></h3>
+                                                            <h3 class="profile-name"><strong>Pigmentação em
+                                                                    barba</strong></h3>
                                                             <br>
                                                             <p class="profile-desc">Descrição (se tiver)</p>
                                                         </div>
@@ -232,7 +295,7 @@
                                                     </div>
                                                 </div>
 
-                                                <div class="card-profile" style="width: 300px;">
+                                                <!-- <div class="card-profile" style="width: 300px;">
                                                     <div class="card-header-profile">
                                                         <center><img
                                                                 src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
@@ -250,65 +313,39 @@
                                                             Profissão: <br>
                                                             Quanto tempo está na área:</p>
                                                     </div>
-                                                </div>
+                                                </div> -->
 
-                                                <div class="card-profile" style="width: 300px;">
-                                                    <div class="card-header-profile">
-                                                        <center><img
-                                                                src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
-                                                                alt=""
-                                                                style="width: 7.5rem; right: 7.5rem; margin-top: -70px; border-radius: 20px">
-                                                        </center>
-                                                        <br><br><br><br>
-                                                        <div class="profile-info">
-                                                            <h3 class="profile-name"><strong>Nome Completo</strong></h3>
+                                                <div class="flex-container space-between">
+                                                    <div class="col card">
+                                                        <div class="img-placeholder">
+                                                            <img
+                                                                src="https://images.unsplash.com/photo-1530797195762-6e542a0f1843?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc5MTY2MjE&ixlib=rb-1.2.1&q=80">
+                                                        </div>
+                                                        <div>
+                                                            <h3>Wafaa</h3>
+                                                            <p><i class="fa-solid fa-user"></i> Front End Web Developer
+                                                            </p>
+                                                            <p><i class="fa-solid fa-paper-plane"></i> <a
+                                                                    href="mailto:wafaa_cr@outlook.com">Email Me</a></p>
+                                                            <p><i class="fa-solid fa-location-dot"></i> Auckland, New
+                                                                Zealand</p>
                                                         </div>
                                                     </div>
-                                                    <div>
-                                                        <p style="font-size: 14px">
-                                                            Idade: <br>
-                                                            Profissão: <br>
-                                                            Quanto tempo está na área:</p>
-                                                    </div>
-                                                </div>
-
-                                                <div class="card-profile" style="width: 300px;">
-                                                    <div class="card-header-profile">
-                                                        <center><img
-                                                                src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
-                                                                alt=""
-                                                                style="width: 7.5rem; right: 7.5rem; margin-top: -70px; border-radius: 20px">
-                                                        </center>
-                                                        <br><br><br><br>
-                                                        <div class="profile-info">
-                                                            <h3 class="profile-name"><strong>Nome Completo</strong></h3>
+                                                    <div class="col card">
+                                                        <div class="img-placeholder">
+                                                            <img
+                                                                src="https://images.unsplash.com/photo-1614130946015-d5a7a3276fea?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc5MTY4MTU&ixlib=rb-1.2.1&q=80">
+                                                        </div>
+                                                        <div>
+                                                            <h3>Wafaa</h3>
+                                                            <p><i class="fa-solid fa-user"></i> Front End Web Developer
+                                                            </p>
+                                                            <p><i class="fa-solid fa-paper-plane"></i> <a
+                                                                    href="mailto:wafaa_cr@outlook.com">Email Me</a></p>
+                                                            <p><i class="fa-solid fa-location-dot"></i> Auckland, New
+                                                                Zealand</p>
                                                         </div>
                                                     </div>
-                                                    <div>
-                                                        <p style="font-size: 14px">
-                                                            Idade: <br>
-                                                            Profissão: <br>
-                                                            Quanto tempo está na área:</p>
-                                                    </div>
-                                                </div>
-
-                                                <div class="card-profile" style="width: 300px;">
-                                                    <div class="card-header-profile">
-                                                        <center><img
-                                                                src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
-                                                                alt=""
-                                                                style="width: 7.5rem; right: 7.5rem; margin-top: -70px; border-radius: 20px">
-                                                        </center>
-                                                        <br><br><br><br>
-                                                        <div class="profile-info">
-                                                            <h3 class="profile-name"><strong>Nome Completo</strong></h3>
-                                                        </div>
-                                                    </div>
-                                                    <p style="font-size: 14px">
-                                                        Idade: <br>
-                                                        Profissão: <br>
-                                                        Quanto tempo está na área:
-                                                    </p>
                                                 </div>
                                             </div>
                                         </fieldset>

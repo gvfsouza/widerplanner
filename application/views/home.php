f38096b771791e6bec29f3733443c2e1d7017650
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-16 08:50:45 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -13,7 +13,7 @@
         border-radius: 1rem;
         box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
         margin: 12px;
-        /* margin-top: 40px; */
+        margin-top: 40px;
     }
 
     .profile-image {

d0926ab36dd62fc99ebf17bed7263000916b9782
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-16 09:55:01 -0300

    .

---
+++ application/views/lista_agendamentos.php
@@ -0,0 +1,34 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Quem Agendou</th>
+      <th scope="col">Data</th>
+      <th scope="col">Hora</th>
+      <th scope="col">Serviço</th>
+      <th scope="col">Profissional</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>

d12d7ad2d2e48f34e2f1de6e1be435a7c6bcebe9
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-18 14:35:23 -0300

    .

--- application/views/lista_agendamentos.php
+++ application/views/lista_agendamentos.php
@@ -169,10 +169,13 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3" style="background-color: white;">
 
-                <div>
-                    <h3 style="font-size: 25px;font-weight:bold;color: #9daed7;">Agendamentos</h3>
-                    <br>
-                </div>
+                <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                    <div>
+                        <legend class="form-group" style="color: white; font-size: 18px;">
+                            <strong>Agendamento</strong>
+                        </legend>
+                    </div>
+                </fieldset>
 
                 <!-- FORMULÁRIO  -->
                 <div class="tab-content" id="myTabContent">

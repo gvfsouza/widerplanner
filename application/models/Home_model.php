62dd60f3d6d3ea23d18c2a91f3f6d55a613f0f3e
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-19 13:25:26 -0300

    .

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -2,11 +2,11 @@
 
 class Home_model extends CI_Model
 {
-    public function listar_estabelecimento($id)
+    public function listar_estabelecimento($idestabelecimento)
     {
         $this->db->select('*');
         $this->db->from('widerplanner.estabelecimento');
-        $this->db->where('id', $id);
+        $this->db->where('id_estabelecimento', $idestabelecimento);
 
         $res = $this->db->get();
         return $res->result();

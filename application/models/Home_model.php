bdeb10b4f6dfa0bcbd69c24bdc0c99ef668324d7
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-20 16:30:50 -0300

    .

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -4,11 +4,11 @@ class Home_model extends CI_Model
 {
     public function listar_estabelecimento()
     {
+        $this->db = $this->load->database('widerplanner', TRUE);
         $this->db->select('*');
         $this->db->from('widerplanner.usuario');
 
         $res = $this->db->get();
         return $res->result();
     }
-    
 }

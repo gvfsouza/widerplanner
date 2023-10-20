1bae7675247e54637c079d0fe32b50777a560f0e
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-10-20 11:27:09 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -468,24 +468,34 @@ a
                                             </div>
                                         </fieldset>
 
-                                        <div class="product-card">
-                                            <div class="badge">Hot</div>
-                                            <div class="product-tumb">
-                                                <img src="https://i.imgur.com/xdbHo4E.png" alt="">
+                                        <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                                            <div>
+                                                <legend class="form-group" style="color: white; font-size: 18px;">
+                                                    <strong>Produtos</strong>
+                                                </legend>
                                             </div>
-                                            <div class="product-details">
-                                                <span class="product-catagory">Women,bag</span>
-                                                <h4><a href="">Women leather bag</a></h4>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
-                                                <div class="product-bottom-details">
-                                                <div class="product-price"><small>$96.00</small>$230.99</div>
-                                                <div class="product-links">
-                                                    <a href=""><i class="fa fa-heart"></i></a>
-                                                    <a href=""><i class="fa fa-shopping-cart"></i></a>
+                                        </fieldset>
+
+                                        <fieldset class="form-group " style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                                            <div class="product-card">
+                                                <div class="badge">Hot</div>
+                                                <div class="product-tumb">
+                                                    <img src="https://i.imgur.com/xdbHo4E.png" alt="">
                                                 </div>
+                                                <div class="product-details">
+                                                    <span class="product-catagory">Women,bag</span>
+                                                    <h4><a href="">Women leather bag</a></h4>
+                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p>
+                                                    <div class="product-bottom-details">
+                                                    <div class="product-price"><small>$96.00</small>$230.99</div>
+                                                    <div class="product-links">
+                                                        <a href=""><i class="fa fa-heart"></i></a>
+                                                        <a href=""><i class="fa fa-shopping-cart"></i></a>
+                                                    </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </fieldset>
                                         <br>
                                     </div>
                                 </div>

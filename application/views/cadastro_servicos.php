b072bb1ffd45e58b294b9cde5e5b970f742b6cc5
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-10-29 22:32:20 -0300

    .

--- application/views/cadastro_servicos.php
+++ application/views/cadastro_servicos.php
@@ -22,14 +22,6 @@
                 <!-- FORMULÁRIO  -->
                 <div class="tab-content" id="myTabContent">
                     <div class="tab-pane fade show active" id="" role="tabpanel">
-
-                        <!-- Mensagem de sucesso cadastro/e-mail  -->
-                        <?php if ($this->session->flashdata('sucesso')) { ?>
-                            <div class="alert alert-success" role="alert">
-                                <?php echo $this->session->flashdata('sucesso'); ?>
-                            </div>
-                        <?php } ?>
-
                         <form action="">
                             <div class="p-3 bg-white">
                                 <div class="row">
@@ -44,32 +36,38 @@
                                                         <div class="form-group col-md-6">
                                                             <label for="" style="color: #4e4e4e;"><b>Foto:</b></label>
                                                             <br>
-                                                            <input style="font-size:14px" type="text" class="form-control" id="foto_servico" name="foto_servico" value="">
+                                                            <input style="font-size:14px" type="text"
+                                                                class="form-control" id="foto_servico"
+                                                                name="foto_servico" value="">
                                                         </div>
                                                         <div class="form-group col-md-6">
-                                                            <label for="" style="color: #4e4e4e;"><b>Nome do Serviço:</b></label>
+                                                            <label for="" style="color: #4e4e4e;"><b>Nome do
+                                                                    Serviço:</b></label>
                                                             <br>
-                                                            <input style="font-size:14px" type="text" class="form-control" id="nome_servico" name="nome_servico" value="">
+                                                            <input style="font-size:14px" type="text"
+                                                                class="form-control" id="nome_servico"
+                                                                name="nome_servico" value="">
                                                         </div>
                                                         <div class="dados-cliente form-group col-md-12">
-                                                            <label for="" style="color: #4e4e4e;"><b>Descrição:</b></label>
+                                                            <label for=""
+                                                                style="color: #4e4e4e;"><b>Descrição:</b></label>
                                                             <br>
-                                                            <textarea class="form-control" name="descricao_servico" id="descricao_servico" cols="5" rows="3"></textarea>
+                                                            <textarea class="form-control" name="descricao_servico"
+                                                                id="descricao_servico" cols="5" rows="3"></textarea>
                                                         </div>
                                                         <div class="dados-cliente form-group col-md-6">
                                                             <label for="" style="color: #4e4e4e;"><b>Valor:</b></label>
                                                             <br>
                                                             <input type="number" class="form-control"
-                                                                aria-label="Amount (to the nearest dollar)" id="valor_servico" name="valor_servico" value="">
-                                                            <div class="input-group-append">
-                                                                <span class="input-group-text">.00</span>
-                                                            </div>
+                                                                id="valor_servico" name="valor_servico" value="">
                                                         </div>
                                                         <div class="dados-cliente form-group col-md-6">
                                                             <label for=""
                                                                 style="color: #4e4e4e;"><b>Duração:</b></label>
                                                             <br>
-                                                            <input style="font-size:14px" type="time" class="form-control" id="duracao_servico" name="duracao_servico" value="">
+                                                            <input style="font-size:14px" type="time"
+                                                                class="form-control" id="duracao_servico"
+                                                                name="duracao_servico" value="">
                                                         </div>
                                                     </div>
                                                 </div>

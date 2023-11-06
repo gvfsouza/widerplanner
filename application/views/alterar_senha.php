fcaba50a8002fdba3e129edede1f0435220407ba
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-06 12:07:01 -0300

    .

--- application/views/alterar_senha.php
+++ application/views/alterar_senha.php
@@ -1,1 +1,80 @@
-<h1>Teste</h1>
+<style>
+label{
+    color:black;
+}
+
+tr, td, th{
+  text-align:center;
+}
+</style>
+
+<br>
+
+<div>
+    <div class="container-fluid">
+      <div class="card shadow mb-4">
+            <div class="card-header py-3" style="background-color: white;">
+
+              <div>
+                <h3 id="dados-cleinte" style="font-size: 25px;font-weight:bold">Alterar senha</h3>
+                <br>
+              </div>
+
+              <!-- FORMULÁRIO  -->
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="" role="tabpanel">
+                  <form action="">
+                    <div class="p-3 bg-white">
+                      <div class="row">
+                        <div class="col-12">
+                          <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                            <div>
+                              <legend class="form-group" style="color: white; font-size: 18px;">
+                                <strong>Dados Pessoais</strong>
+                              </legend>
+                            </div>
+                          </fieldset>
+                          <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                            <div class="row">
+                              <div style="margin-left:10px;"></div>
+
+                              <div class="col">
+                                
+                                <div class="row">
+                                  <div class="form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Senha atual:</label>
+                                    <br>
+                                    <input style="font-size:14px" type="text" class="form-control" id="senha_atual" name="senha_atual" value="" disabled="">
+                                  </div>
+                                  <div class="dados-cliente form-group col-md-6">
+                                    <label for="" style="color: #77787a;">Nova senha:</label>
+                                    <br>
+                                    <input type="text" class="form-control" id="nova_senha" name="nova_senha" disabled="">
+                                  </div>
+                                  <div class="dados-cliente form-group col-md-4">
+                                    <label for="" style="color: #77787a;">Confirmação da senha</label>
+                                    <br>
+                                    <input type="date" class="form-control" id="conf_senha" name="conf_senha" value="" disabled="">
+                                  </div>
+                              </div>
+                            </div>
+                          </fieldset>
+                          <br>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              
+              <div class="col-12 text-center">
+                <span style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
+                  Projeto de Conclusão de Curso - Superior de Tecnologia em Análise e Deselvolvimento de Sistemas
+                  <br>
+                  Centro Universitário Nossa Senhora do Patrocínio <br>
+                  Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>

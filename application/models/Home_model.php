1c43b033c8b01c52caac3565635c98743f0c72c7
emersseu <126602427+emersseu@users.noreply.github.com>
2023-11-06 16:48:59 -0300

    teste

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -5,7 +5,7 @@ class Home_model extends CI_Model
     public function listar_estabelecimento()
     {
         $this->db->select('*');
-        $this->db->from('widerplanner.usuario');
+        $this->db->from('usuario');
 
         $res = $this->db->get();
         return $res->result();

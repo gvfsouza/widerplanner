bd21cb7c84347ac1df97d33d940342cfdb5fc976
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-07 11:32:52 -0300

    .

--- application/views/login.php
+++ application/views/login.php
@@ -1,129 +1,150 @@
 <style>
-  html,
-  body {
-    height: 100%;
+  @import url("https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@100;300;400;500;700;900&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap");
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "poppins", sans-serif;
   }
 
   body {
-    background-color: #508fc03d;
-  }
 
-  #rodape {
-    text-align: center;
-  }
-
-  label {
-    margin: auto;
-    text-align: center;
-  }
-
-  .form-control {
-
-    margin: auto;
-  }
-
-  .form-group {
-    max-width: 400px;
-    margin: 0 auto;
-  }
-
-  /* Resolução para celular */
-  @media (max-width: 300px) {
-    .form-control {
-      width: 100%;
-      margin: auto;
-    }
-
-    label {
-      margin: auto;
-      margin-left: 0%;
-    }
-
-    .form-control p img {
-      width: 100%;
-      margin: auto;
-
-    }
-
-  }
-
-  div .form-group input:valid-i {
-    display: block;
-  }
-
-  div .form-group i.hide-btn::defore {
-    content: "\f070";
-  }
-
-  div .form-group i #show-btn {
-    float: right;
-
-    margin: 3px 0px 0px -20px;
-    cursor: pointer
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #879bc942;
   }
 
   .container {
+    background-color: white;
+    padding: 6px;
+    width: 350px;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     position: relative;
-    /* display: flex;
-        flex-direction: row;
-        justify-content: middle;
-        align-items: middle; */
-    max-width: 1000px;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: 0 0 20px 0 darkgrey;
+  }
+
+  form {
+    position: absolute;
+    /* margin-top: 80px; */
+    transition: 0.5s;
+    width: 300px;
+    height: 400px;
+  }
+
+  .buttonsForm {
+    margin-top: 40px;
+    position: relative;
+    border-radius: 5px;
+    box-shadow: 0 0 7px 0 #879bc9;
+  }
+
+  .buttonsForm button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    position: relative;
+    padding: 10px 29px;
+  }
+
+  .btnColor {
+    position: absolute;
+    width: 108px;
+    height: 100%;
+    background: #879bc9;
+    border-radius: 5px;
+    transition: 0.3s;
+  }
+
+  input[type="text"],
+  input[type="password"] {
+    border: none;
+    margin-top: 10px;
+    border-radius: 5px;
+    padding: 8px 0 8px 35px;
+    outline: none;
+    width: 100%;
+    box-shadow: 0 0 7px 0 #879bc994;
+  }
+
+  button[type="submit"] {
+    background: #879bc9;
+    color: white;
+    border-radius: 5px;
+    width: 100%;
+    border: none;
+    outline: none;
+    padding: 8px 0 8px 15px;
+    font-size: 15px;
+    margin-top: 40px;
+    cursor: pointer;
+  }
+
+  .divCheck {
+    margin: 10px 0 0 5px;
+    width: 80%;
+    font-size: 13px;
+  }
+
+  i {
+    position: absolute;
+    left: 0;
+    padding: 10px 10px;
+  }
+
+  .iPassword {
+    top: 114px;
+  }
+
+  .iPassword2 {
+    top: 190px;
+  }
+
+  label{
+    color: #879BC9;
+  }
+
+  a{
+    text-decoration: none;
   }
 </style>
 
 <div class="container">
-  <!-- Outer Row -->
-  <div class="row justify-content-center d-flex align-items-center h-100">
-
-    <div class="col-xl-6 col-lg-12 col-md-9 ">
-
-      <div class="card o-hidden border-0 shadow-lg my-5 ">
-        <div class="card-body p-0">
-
-          <!-- Nested Row within Card Body -->
-          <div class="row ">
-            <div class="col-lg-12">
-              <div class="form-group">
-                <div class="p-3 ">
-
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-800 mb-4" style="font-size: 18px;"><b>WiderPlanner
-                        <br>
-                        LOGIN</b></h1>
-                  </div>
-                  <form method="POST" action="">
-                    <div class="form-group">
-                      <label for="cpf"><i class="fas fa-id-card"></i> CPF</label>
-                      <input type="number" name="CPF" maxlength="14" class="form-control" required>
-                    </div>
-                    <br>
-
-                    <div class="form-group" style="position: relative;">
-                      <label for="senha"><i class="fas fa-lock"></i> Senha</label>
-                      <input type="password" class="form-control" placeholder="Insira sua senha" name="senha" id="senha" required>
-                      <!-- <i class="fas fa-solid fa-eye-slash" id="show-btn" style="position: absolute; cursor:pointer; top: 37px; right: 7px;"></i> -->
-                    </div>
-                    <br>
-
-                    <a style="text-align:center; color:#135A93" href="">Esqueci senha</a>
-                    <br>
-                    <span><a href=""><strong style="font-size:14px">Clique aqui</strong> para realizar o cadastro</a></span>
-
-                    <br>
-                    <p style="text-align: right;"><input type="hidden" name="enviar" id="enviar" value="1" /> <input class="form-control" style="background:#135A93; color:white" type="submit" name="entrar" id="entrar" value="Entrar" /></p>
-                  </form>
-                  <div id="rodape">
-                    <p><small>&copy; Sistema desenvolvido para cunho estudantil <br /> Desenvolvimento Web: Departamento de Informática <br /> Projeto de Conclusão de Curso - Superior de
-                        Tecnologia em Análise e Deselvolvimento de Sistemas <br>Centro Universitário Nossa Senhora do
-                        Patrocínio<br>Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</small></p>
-                  </div>
-                </div>
-              </div>
+  <form id="signin">
+    <div class="row">
+        <div class="col-md-12">
+          <p style="text-align:center;font-size: 31px;color: #879bc9;font-weight: bold;">Login</p>
+          <br>
+            <div class="form-group">
+                <label for="cpf"> <i class="fas fa-lock"></i> CPF:</label>
+                <input type="text" class="form-control" placeholder="Digite o CPF" name="cpf" id="cpf">
             </div>
-          </div>
         </div>
-      </div>
+        <br>
+        <div class="col-md-12">
+            <label for="senha"> <i class="fas fa-lock"></i> Senha:</label>
+            <input type="password" class="form-control" placeholder="Insira sua senha" name="senha" id="senha">
+        </div>
     </div>
+    <div class="divCheck">
+      <br>
+      <span><a href="">Esqueci Senha</a></span>
+      <br>
+      <br>
+      <span><a href=""><strong style="font-size:14px">Clique aqui</strong> para realizar o cadastro</a></span>
+    </div>
+    <button type="submit">Entrar</button>
+  </form>
+  <div class="col-12 text-center">
+    <span class="text-center" style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
+        Projeto de Conclusão de Curso - Superior de
+        Tecnologia em Análise e Deselvolvimento de Sistemas <br> Centro Universitário Nossa Senhora do
+        Patrocínio <br> Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
   </div>
 </div>

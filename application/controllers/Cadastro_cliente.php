045e71256afb9f61a41d573df76ec8b790273622
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-07 08:11:18 -0300

    .

--- application/controllers/Cadastro_cliente.php
+++ application/controllers/Cadastro_cliente.php
@@ -53,7 +53,7 @@ class Cadastro_cliente extends CI_Controller
 				$dados['cadastro_cliente'] = $this->Cliente_model->cadastro_cliente($nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario, $complemento_usuario, $bairro_usuario, $cidade_usuario, $uf_usuario,$senha_criptografada);
 
 				$this->load->library('email');
-				$this->email->from('remetente: contato@agsete.com.br','widerplanner');
+				$this->email->from('contato@agsete.com.br','widerplanner');
 				$this->email->to($email_usuario); // Endereço de e-mail do usuário
 				$this->email->subject('WiderPlanner - Dados de acesso ao sistema');
 				$this->email->message('Sua nova senha de acesso é: ' . $password);

1c0244f73a106750f3fc9dd636bf12e55a1f2a86
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-14 12:25:24 -0300

    .

--- application/controllers/Cadastro_produto.php
+++ application/controllers/Cadastro_produto.php
@@ -1,16 +1,25 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cadastro_produto extends CI_Controller {
+class Cadastro_produto extends CI_Controller 
+{
 	public function __construct()
 	{
-	  parent::__construct();
-		
-	  if (isset($_POST)) {
-		if (!empty($_POST)) {
-		  anti_injection($_POST);
+		parent::__construct();
+		// Carregue o arquivo functions_helper
+		$this->load->helper('functions_helper');
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$_POST = $this->clean_post_data($_POST);
 		}
-	  }
+		$this->load->database();
+	}
+
+	private function clean_post_data($data)
+	{
+		foreach ($data as $key => $value) {
+			$data[$key] = anti_injection($value);
+		}
+		return $data;
 	}
 	
 	public function index()

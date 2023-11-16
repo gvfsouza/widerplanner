0b81b5b10e0f9c13a78ff54bb12898f274e954aa
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-16 13:30:57 -0300

    .

--- application/controllers/Cadastro_cliente.php
+++ application/controllers/Cadastro_cliente.php
@@ -52,7 +52,7 @@ class Cadastro_cliente extends CI_Controller
             $linkAcesso = 'https://widerplanner.agsete.com.br/login'; // Link para a página de login
           
             if (!isset($error)) {
-                $dados['cadastro_cliente'] = $this->Cliente_model->cadastro_cliente($nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario, $complemento_usuario, $bairro_usuario, $cidade_usuario, $uf_usuario, $senha_criptografada);
+               $this->Cliente_model->cadastro_cliente($nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario, $complemento_usuario, $bairro_usuario, $cidade_usuario, $uf_usuario, $senha_criptografada);
 
                 $this->load->library('email');
                 $config['mailtype'] = 'html';
@@ -88,6 +88,6 @@ class Cadastro_cliente extends CI_Controller
         }
 
         $this->load->view('layout/header');
-        $this->load->view('cadastro_cliente', $dados);
+        $this->load->view('cadastro_cliente');
     }
 }

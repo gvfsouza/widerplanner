29a0dcb6364e8ac630de25e810c997801feb5346
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-21 12:15:18 -0300

    .

--- application/views/login.php
+++ application/views/login.php
@@ -76,6 +76,10 @@
   .form-control{
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
   }
+
+  input::placeholder {
+    color: gray; /* Substitua com a cor desejada */
+}
 </style>
 
 <body>

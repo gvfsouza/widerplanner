01d350b9417d64920ff789f2508484139a2b54f1
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-21 12:11:44 -0300

    .

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,7 +1,7 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller 
+class Login extends CI_Controller
 {
 	public function __construct()
 	{
@@ -21,10 +21,10 @@ class Login extends CI_Controller
 		}
 		return $data;
 	}
-	
+
 	public function index()
 	{
-        $this->load->model('Login_model');
+		$this->load->model('Login_model');
 
 		if (isset($_POST['Entrar'])) {
 
@@ -36,8 +36,9 @@ class Login extends CI_Controller
 		$this->load->view('login');
 	}
 
-	public function login() {
-        $this->load->model('Login_model');
+	public function login()
+	{
+		$this->load->model('Login_model');
 
 		$senha = $this->security->xss_clean($this->input->post('senha'));
 		$cpf_usuario = $this->security->xss_clean($this->input->post('cpf_usuario'));
@@ -46,13 +47,13 @@ class Login extends CI_Controller
 		if ($this->session->userdata('logged_in')) {
 			redirect('home');
 		}
-	
+
 		$this->load->library('form_validation');
-		
+
 		// Regras de validação
 		$this->form_validation->set_rules('cpf_usuario', 'CPF do Usuário', 'trim|required');
 		$this->form_validation->set_rules('senha', 'Senha', 'trim|required');
-	
+
 		if ($this->form_validation->run() == FALSE) {
 			// Carrega a visualização de login com erros de validação
 			$this->load->view('login');
@@ -60,19 +61,21 @@ class Login extends CI_Controller
 			// Valida as credenciais do usuário
 			$cpf_usuario = $this->input->post('cpf_usuario');
 			$senha = $this->input->post('senha');
-	
+
 			$user = $this->Login_model->autenticacao_usuario($cpf_usuario, $senha);
-	
+
 			if ($user) {
 				// Configura dados da sessão do usuário e redireciona para o painel
 				$this->session->set_flashdata('sucesso', 'Login efetuado com sucesso!');
+				$this->load->view('home');
 				redirect('home');
 			} else {
 				// Login inválido, exibe mensagem de erro
 				$this->session->set_flashdata('error', 'CPF do usuário ou senha inválidos');
+				$this->load->view('login');
 				redirect('login');
 			}
 		}
 	}
-	
+
 }

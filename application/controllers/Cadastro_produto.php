acbd2f7ed60cced9399d928d03fb200618661bad
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-20 22:47:04 -0300

    .

--- application/controllers/Cadastro_produto.php
+++ application/controllers/Cadastro_produto.php
@@ -34,40 +34,14 @@ class Cadastro_produto extends CI_Controller
 			$valor_produto = $this->input->post('valor_produto');
 			$duracao_produto = $this->input->post('duracao_produto');
 
-			$config['upload_path']          = './arquivos/';
-			$config['allowed_types']        = '*';
-			$config['allowed_types']        = 'jpg|jpeg|png';
-			$config['max_size']             = 0;
-			// $config['max_width']            = 1024;
-			// $config['max_height']           = 768;
-			$config['encrypt_name'] = TRUE;
-			$this->load->library('upload', $config);
-		
-			for ($i = 0; $i < count($foto_servico['name']); $i++) {
-				if ($foto_servico['size'][$i] == "") {
-					$foto_servico[] = "";
-				} else {
-					$_FILES['arquivo']['name'] = $foto_produto['name'][$i];
-					$_FILES['arquivo']['type'] = $foto_produto['type'][$i];
-					$_FILES['arquivo']['tmp_name'] = $foto_produto['tmp_name'][$i];
-					$_FILES['arquivo']['error'] = $foto_produto['error'][$i];
-					$_FILES['arquivo']['size'] = $foto_produto['size'][$i];
-		
-					if (!$this->upload->do_upload('arquivo')) {
-						$error = array('erro' => $this->upload->display_errors());
-						$this->session->set_flashdata('erro', $error['erro']);
-					} else {
-						$arquivo_[] = $this->upload->data('file_name');
-					}
-				}
 			}
 
 			if (!isset($error)) {
-				$dados['cadastro_servicos'] = $this->Servicos_model->cadastro_servicos($foto_produto, $nome_produto, $descricao_produto, $valor_produto, $qtde_produto);
+				$dados['cadastro_produto'] = $this->Servicos_model->cadastro_servicos($foto_produto, $nome_produto, $descricao_produto, $valor_produto, $qtde_produto);
 
 				//MENSAGEM SUCESSO AO CADASTRAR
 				$this->session->set_flashdata('sucesso', 'Cadastro de Produto realizado com sucesso!');
-				redirect('cadastro_servicos');
+				redirect('cadastro_produto');
 			} else {
 				$this->session->set_flashdata('erro', 'Erro ao efetuar cadastro de Produto.');
 			}

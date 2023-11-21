ce437c6600750b47c74510b8e2bef25a52908f83
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-21 09:34:21 -0300

    .

--- application/controllers/Cadastro_produto.php
+++ application/controllers/Cadastro_produto.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Cadastro_funcionario extends CI_Controller
+class Cadastro_Produto extends CI_Controller
 {
 	public function __construct()
 	{

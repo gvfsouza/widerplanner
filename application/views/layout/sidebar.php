a5f2524a8d940a312abd7a81987f8d7619379484
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-21 16:29:35 -0300

    .

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -108,3 +108,8 @@
    <!-- Content Wrapper -->
    
    <div id="content-wrapper" class="d-flex flex-column" style="background-color: #d9e2f7">
+   <script>
+    $(document).ready(function() {
+        $('#logoutModal').modal('show');
+    });
+</script>

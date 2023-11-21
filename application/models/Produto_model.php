72f6f2aa6ac12131caa50e292db62884dea3c67e
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-21 08:53:00 -0300

    .

--- application/models/Produto_model.php
+++ application/models/Produto_model.php
@@ -1,6 +1,8 @@
-<?php if(!defined('BASEPATH')) exit('No direct scripts access allowed');
+<?php if (!defined('BASEPATH'))
+    exit('No direct scripts access allowed');
 
-class Produto_model extends CI_Model {
+class Produto_model extends CI_Model
+{
 
     public function listar_produtos()
     {
@@ -13,17 +15,22 @@ class Produto_model extends CI_Model {
 
     public function cadastro_produto($foto_produto, $nome_produto, $descricao_produto, $valor_produto, $qtde_produto)
     {
-        $data = array(
-            # puxa os campos do banco
-            'foto_produto' => $foto_produto,
-            'nome_produto' => $nome_produto,
-            'descricao_produto' => $descricao_produto,
-            'valor_produto' => $valor_produto,
-            'qtde_produto' => $qtde_produto,
-        );
+        // Adicione uma verificação para garantir que 'foto_produto' não seja nulo
+        if ($foto_produto !== null) {
+            $data = array(
+                'foto_produto' => $foto_produto,
+                'nome_produto' => $nome_produto,
+                'descricao_produto' => $descricao_produto,
+                'valor_produto' => $valor_produto,
+                'qtde_produto' => $qtde_produto,
+            );
 
-
-        $this->db->insert('produtos', $data);
-        return $this->db->insert_id();
+            $this->db->insert('produtos', $data);
+            return $this->db->insert_id();
+            
+        } else {
+            // Trate o caso em que 'foto_produto' é nulo
+            return false;
+        }
     }
 }

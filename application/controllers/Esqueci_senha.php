d66dd2193655732215b0cd0868ec8d2fd55ecf34
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-23 08:27:42 -0300

    .

--- application/controllers/Esqueci_senha.php
+++ application/controllers/Esqueci_senha.php
@@ -24,39 +24,40 @@ class Esqueci_senha extends CI_Controller
 	public function index()
 	{
 
-    $this->load->model('Login_model');
+		$this->load->model('Login_model');
 
-    if (isset($_POST['enviar'])) {
+		if (isset($_POST['enviar'])) {
 
-        $this->load->library('encryption');
+			$this->load->library('encryption');
 
-        $cpf_usuario = isset($_POST['cpf_usuario']) ? $_POST['cpf_usuario'] : null;
-        $email_usuario = isset($_POST['email_usuario']) ? $_POST['email_usuario'] : null;
+			$cpf_usuario = isset($_POST['cpf_usuario']) ? trim($_POST['cpf_usuario']) : null;
+			$email_usuario = isset($_POST['email_usuario']) ? trim($_POST['email_usuario']) : null;
 
-        if (empty($cpf_usuario) || empty($email_usuario)) {
-            $this->session->set_flashdata('error', 'Todos os campos devem ser preenchidos.');
-        } else {
-            // Removida a verificação de $error, pois não é usada
-            $dados = $this->Login_model->esqueci_senha($cpf_usuario, $email_usuario);
 
-            if ($dados) {
-                $link_esqueci_senha = base_url() . 'esqueci_senha/recuperar_senha/' . $dados->chave_recuperacao;
+			if ($cpf_usuario === null || $cpf_usuario === '' || $email_usuario === null || $email_usuario === '') {
+				$this->session->set_flashdata('error', 'Todos os campos devem ser preenchidos.');
+			} else {
+				// Removida a verificação de $error, pois não é usada
+				$dados = $this->Login_model->esqueci_senha($cpf_usuario, $email_usuario);
 
-                // Restante do código do envio de e-mail aqui
+				if ($dados) {
+					$link_esqueci_senha = base_url() . 'esqueci_senha/recuperar_senha/' . $dados->chave_recuperacao;
 
-                $this->session->set_flashdata('success_email', 'Verifique sua caixa de e-mails para recuperar a senha');
-            } else {
-                $this->session->set_flashdata('error', 'Usuário não encontrado.');
-            }
-        }
-    }
+					// Restante do código do envio de e-mail aqui
 
-    // Descomente esta linha se desejar redirecionar após o envio do e-mail
-    // redirect('esqueci_senha');
+					$this->session->set_flashdata('success_email', 'Verifique sua caixa de e-mails para recuperar a senha');
+				} else {
+					$this->session->set_flashdata('error', 'Usuário não encontrado.');
+				}
+			}
+		}
 
-    $this->load->view('layout/header');
-    $this->load->view('esqueci_senha');
-}
+		// Descomente esta linha se desejar redirecionar após o envio do e-mail
+		// redirect('esqueci_senha');
+
+		$this->load->view('layout/header');
+		$this->load->view('esqueci_senha');
+	}
 
 	public function recuperar_senha()
 	{

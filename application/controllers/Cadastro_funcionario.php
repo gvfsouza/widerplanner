648861c7317fe39a903c8326eb5b35ed075ffc05
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-24 07:57:48 -0300

    .

--- application/controllers/Cadastro_funcionario.php
+++ application/controllers/Cadastro_funcionario.php
@@ -44,23 +44,7 @@ class Cadastro_funcionario extends CI_Controller
 
 			$Servicos_selecionados = $this->input->post('fk_servico');
 
-			$id_usuario = $this->Funcionario_model->cadastro_profissional(
-				$foto_usuario,
-				$nome_usuario,
-				$cpf_usuario,
-				$dt_nasc_usuario,
-				$email_usuario,
-				$sexo_usuario,
-				$telefone_usuario,
-				$cep_usuario,
-				$logradouro_usuario,
-				$numero_usuario,
-				$complemento_usuario,
-				$bairro_usuario,
-				$cidade_usuario,
-				$estado_usuario,
-				md5(gerarSenhaAleatoria(8))
-			);
+			$id_usuario = $this->Funcionario_model->cadastro_profissional($foto_usuario, $nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario, $complemento_usuario, $bairro_usuario, $cidade_usuario, $estado_usuario, md5(gerarSenhaAleatoria(8)));
 
 			// Insert selected services into func_servicos table
 			foreach ($Servicos_selecionados as $id_servicos) {

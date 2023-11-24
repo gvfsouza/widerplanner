d11829a86dfd78755ba992f748fdccc9ff62540d
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-24 11:54:45 -0300

    .

--- application/models/Funcionario_model.php
+++ application/models/Funcionario_model.php
@@ -5,6 +5,10 @@ class Funcionario_model extends CI_Model
 {
     public function cadastro_profissional($foto_usuario, $nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario, $complemento_usuario, $bairro_usuario, $cidade_usuario, $estado_usuario, $fk_servico, $senha_criptografada)
     {
+        if ($foto_usuario == '') {
+            $foto_usuario = NULL;
+        }
+
         $data = array(
             # puxa os campos do banco
             'foto_usuario' => $foto_usuario,

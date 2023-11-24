a79003f01561959286b2a9a9b0bbfc72eb71119d
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-24 13:37:48 -0300

    .

--- application/views/perfil_cliente.php
+++ application/views/perfil_cliente.php
@@ -24,146 +24,163 @@
                             <div class="p-3 bg-white">
                                 <div class="row">
                                     <div class="col-12">
-                                        <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                                        <fieldset class="form-group"
+                                            style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
                                             <div>
                                                 <legend class="form-group" style="color: white; font-size: 18px;">
                                                     <strong>Perfil Cliente</strong>
                                                 </legend>
                                             </div>
                                         </fieldset>
-                                        <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                                        <fieldset class="form-group"
+                                            style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
                                             <div class="row">
                                                 <div style="margin-left:10px;"></div>
 
                                                 <div class="col">
 
                                                     <div class="row">
-                                                        <legend class="form-group" style="color: white; font-size: 18px; margin-left:14px">
+                                                        <legend class="form-group"
+                                                            style="color: #879bc9; font-size: 18px; margin-left:14px">
                                                             <strong>Dados Pessoais</strong>
                                                         </legend <div class="form-group col-md-6">
+
                                                         <label for="" style="color: #77787a;">Nome:</label>
                                                         <br>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="nome" name="nome" value="" disabled="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="nome" name="nome" value="" disabled="">
                                                     </div>
                                                     <div class="dados-cliente form-group col-md-6">
                                                         <label for="" style="color: #77787a;">CPF:</label>
                                                         <br>
-                                                        <input type="text" class="form-control" id="cpd" name="cpf" disabled="">
+                                                        <input type="text" class="form-control" id="cpd" name="cpf"
+                                                            disabled="">
                                                     </div>
                                                     <div class="dados-cliente form-group col-md-4">
                                                         <label for="" style="color: #77787a;">Data Nasc.:</label>
                                                         <br>
-                                                        <input type="date" class="form-control" id="dt_nascimento" name="dt_nascimento" value="" disabled="">
+                                                        <input type="date" class="form-control" id="dt_nascimento"
+                                                            name="dt_nascimento" value="" disabled="">
                                                     </div>
                                                     <div class="dados-cliente form-group col-md-2">
                                                         <label for="" style="color: #77787a;">Sexo:</label>
                                                         <br>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="sexo" name="sexo" value="" disabled="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="sexo" name="sexo" value="" disabled="">
                                                     </div>
                                                     <div class="dados-cliente col-md-6">
                                                         <label for="" style="color: #77787a;">Celular:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="celular" name="celular" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="celular" name="celular" value="">
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div class="dados-cliente col-md-3">
                                                         <label for="" style="color: #77787a;">CEP:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="cep" name="cep" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="cep" name="cep" value="">
                                                     </div>
                                                     <div class="dados-cliente col-md-7">
                                                         <label for="" style="color: #77787a;">Logradouro:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="logradouro" name="logradouro" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="logradouro" name="logradouro" value="">
                                                     </div>
                                                     <div class="dados-cliente col-md-2">
                                                         <label for="" style="color: #77787a;">N°:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="numero" name="numero" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="numero" name="numero" value="">
                                                     </div>
                                                 </div><br>
                                                 <div class="row">
                                                     <div class="dados-cliente col-md-5">
                                                         <label for="" style="color: #77787a;">Complemento:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="complemento" name="complemento" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="complemento" name="complemento" value="">
                                                     </div>
                                                     <div class="dados-cliente col-md-7">
                                                         <label for="" style="color: #77787a;">Bairro:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="bairro" name="bairro" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="bairro" name="bairro" value="">
                                                     </div>
                                                 </div><br>
                                                 <div class="row">
                                                     <div class="dados-cliente col-md-7">
                                                         <label for="" style="color: #77787a;">Cidade:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="cidade" name="cidade" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="cidade" name="cidade" value="">
                                                     </div>
                                                     <div class="dados-cliente col-md-5">
                                                         <label for="" style="color: #77787a;">UF:</label>
-                                                        <input style="font-size:14px" type="text" class="form-control" id="uf" name="uf" value="">
+                                                        <input style="font-size:14px" type="text" class="form-control"
+                                                            id="uf" name="uf" value="">
                                                     </div>
                                                 </div>
                                             </div>
+                                        </fieldset>
                                     </div>
-                                    </fieldset>
-                                    <br>
                                 </div>
                             </div>
+                        </form>
                     </div>
-                    </form>
                 </div>
-            </div>
-            <form action="">
-                <div class="p-3 bg-white">
-                    <div class="row">
-                        <div class="col-12">
-                            <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
-                                <div>
-                                    <legend class="form-group" style="color: white; font-size: 18px;"> <i class="fas fa-clipboard-list"></i>
-                                        <strong>Histórico</strong>
-                                    </legend>
-                                </div>
-                            </fieldset>
+                <form action="">
+                    <div class="p-3 bg-white">
+                        <div class="row">
+                            <div class="col-12">
+                                <fieldset class="form-group"
+                                    style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                                    <div>
+                                        <legend class="form-group" style="color: white; font-size: 18px;"> <i
+                                                class="fas fa-clipboard-list"></i>
+                                            <strong>Histórico</strong>
+                                        </legend>
+                                    </div>
+                                </fieldset>
+                            </div>
                         </div>
+                        <fieldset class="form-group"
+                            style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;color: black;">
+                            <table class="table table-responsive-lg">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" style="font-size: 13px;">Serviço</th>
+                                        <th scope="col" style="font-size: 13px;">Data</th>
+                                        <th scope="col" style="font-size: 13px;">Hora</th>
+                                        <th scope="col" style="font-size: 13px;">Profissional</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td style="font-size: 11px;">Corte Masculino</td>
+                                        <td style="font-size: 11px;">05/03/23</td>
+                                        <td style="font-size: 11px;">14h 30min</td>
+                                        <td style="font-size: 11px;">Cristian</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="font-size: 11px;">Pigmetação</td>
+                                        <td style="font-size: 11px;">12/02/23</td>
+                                        <td style="font-size: 11px;">17h00</td>
+                                        <td style="font-size: 11px;">Thiago</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="font-size: 11px;">Luzes</td>
+                                        <td style="font-size: 11px;">07/01/23</td>
+                                        <td style="font-size: 11px;">15h00</td>
+                                        <td style="font-size: 11px;">Giovane</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </fieldset>
                     </div>
-                    <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;color: black;">
-                        <table class="table table-responsive-lg">
-                            <thead>
-                                <tr>
-                                    <th scope="col" style="font-size: 13px;">Serviço</th>
-                                    <th scope="col" style="font-size: 13px;">Data</th>
-                                    <th scope="col" style="font-size: 13px;">Hora</th>
-                                    <th scope="col" style="font-size: 13px;">Profissional</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td style="font-size: 11px;">Corte Masculino</td>
-                                    <td style="font-size: 11px;">05/03/23</td>
-                                    <td style="font-size: 11px;">14h 30min</td>
-                                    <td style="font-size: 11px;">Cristian</td>
-                                </tr>
-                                <tr>
-                                    <td style="font-size: 11px;">Pigmetação</td>
-                                    <td style="font-size: 11px;">12/02/23</td>
-                                    <td style="font-size: 11px;">17h00</td>
-                                    <td style="font-size: 11px;">Thiago</td>
-                                </tr>
-                                <tr>
-                                    <td style="font-size: 11px;">Luzes</td>
-                                    <td style="font-size: 11px;">07/01/23</td>
-                                    <td style="font-size: 11px;">15h00</td>
-                                    <td style="font-size: 11px;">Giovane</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </fieldset>
+                </form>
+                <div class="col-12 text-center">
+                    <span style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
+                        Projeto de Conclusão de Curso - Superior de Tecnologia em Análise e Deselvolvimento de Sistemas
+                        <br>
+                        Centro Universitário Nossa Senhora do Patrocínio <br>
+                        Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
                 </div>
-            </form>
-            <div class="col-12 text-center">
-                <span style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
-                    Projeto de Conclusão de Curso - Superior de Tecnologia em Análise e Deselvolvimento de Sistemas
-                    <br>
-                    Centro Universitário Nossa Senhora do Patrocínio <br>
-                    Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
             </div>
         </div>
     </div>
-</div>
 </div>

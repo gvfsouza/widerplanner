bbc3f724289b0e0d21294b80b4b2377b11676cb4
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-27 12:53:25 -0300

    .

--- application/views/agendamento.php
+++ application/views/agendamento.php
@@ -44,51 +44,56 @@
               <div class="p-3 bg-white">
                 <div class="row">
                   <div class="col-12">
-                    <div class="row">
-                      <div class="form-group col-md-6">
-                        <label for="" style="color: #4e4e4e;"><b>Escolha uma Data:</b></label>
-                        <br>
-                        <input type="date" class="form-control" id="fk_dia_semana" name="fk_dia_semana" max="9999-12-31">
-                        <!-- <select name="fk_dia_semana" id="fk_dia_semana" class="form-control fk_dia_semana" style="cursor: pointer;" required>
+                    <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                      <div class="row">
+                        <div class="form-group col-md-6">
+                          <label for="" style="color: #4e4e4e;"><b>Escolha uma Data:</b></label>
+                          <br>
+                          <input type="date" class="form-control" id="fk_dia_semana" name="fk_dia_semana" max="9999-12-31">
+                          <!-- <select name="fk_dia_semana" id="fk_dia_semana" class="form-control fk_dia_semana" style="cursor: pointer;" required>
                           <option class="text-center" value="">--- Selecione uma Opção ---</option>
                           </?php foreach ($listar_diasemana as $value) { ?>
                                 <option value="</?php echo $value->id_semana; ?>" data-nome="</?php echo $value->dias_semana; ?>"></?php echo $value->dias_semana; ?></option>
                           </?php } ?>
                         </select> -->
-                      </div>
+                        </div>
 
-                      <div class="form-group col-md-6">
-                        <label for="" style="color: #4e4e4e;"><b>Escolha um Horário:</b></label>
-                        <br>
-                        <select name="fk_hora" id="fk_hora" class="form-control servico" style="cursor: pointer;" required>
-                          <option class="text-center" value="">--- Selecione uma Opção ---</option>
-                          <?php foreach ($listar_hora as $value) { ?>
-                                    <option value="<?php echo $value->id_hora; ?>" data-nome="<?php echo $value->horarios_semana; ?>"><?php echo $value->horarios_semana; ?></option>
-                          <?php } ?>
-                        </select>
+                        <div class="form-group col-md-6">
+                          <label for="" style="color: #4e4e4e;"><b>Escolha um Horário:</b></label>
+                          <br>
+                          <select name="fk_hora" id="fk_hora" class="form-control servico" style="cursor: pointer;" required>
+                            <option class="text-center" value="">--- Selecione uma Opção ---</option>
+                            <?php foreach ($listar_hora as $value) { ?>
+                                  <option value="<?php echo $value->id_hora; ?>" data-nome="<?php echo $value->horarios_semana; ?>"><?php echo $value->horarios_semana; ?></option>
+                            <?php } ?>
+                          </select>
+                        </div>
                       </div>
-                    </div>
+                    </fieldset>
 
                     <br>
 
-                    <div class="row">
-                      <div class="col-md-4">
-                        <label for="" style="color: #4e4e4e;"><b>Serviço:</b></label>
-                        <br>
-                        <select name="fk_servicos[]" id="fk_servicos" class="form-control servico" style="cursor: pointer;" required>
-                          <option class="text-center" value="">--- Selecione uma Opção ---</option>
-                          <?php foreach ($listar_servicos as $value) { ?>
-                                    <option value="<?php echo $value->id_servicos; ?>" data-nome="<?php echo $value->nome_servico; ?>"><?php echo $value->nome_servico; ?></option>
-                          <?php } ?>
-                        </select>
-                      </div>
-                      <div class="col-md-3" style="margin-top: 30px;">
-                        <div class="servicos_add">
-                          <button type="button" class="btn btn-primary add_novo_Servico" style="width: 185px; height: 40px; font-size: 14px;"><i class="fas fa-plus"></i> Adicionar Serviço</button>
-                          <button type="button" class="btn btn-danger botao_remover" style="width: 150px; height: 40px; font-size: 14px; margin-left: 10px;"><i class="fas fa-trash-alt"></i> Remover</button>
+                    <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
+                      <div class="row">
+                        <div class="col-md-4">
+                          <label for="" style="color: #4e4e4e;"><b>Serviço:</b></label>
+                          <br>
+                          <select name="fk_servicos[]" id="fk_servicos" class="form-control servico" style="cursor: pointer;" required>
+                            <option class="text-center" value="">--- Selecione uma Opção ---</option>
+                            <?php foreach ($listar_servicos as $value) { ?>
+                                  <option value="<?php echo $value->id_servicos; ?>" data-nome="<?php echo $value->nome_servico; ?>"><?php echo $value->nome_servico; ?></option>
+                            <?php } ?>
+                          </select>
                         </div>
-                      </div>
+                        <div class="col-md-3" style="margin-top: 30px;">
+                          <div class="servicos_add">
+                            <button type="button" class="btn btn-primary add_novo_Servico" style="width: 185px; height: 40px; font-size: 14px;"><i class="fas fa-plus"></i> Adicionar Serviço</button>
+                            <button type="button" class="btn btn-danger botao_remover" style="width: 150px; height: 40px; font-size: 14px; margin-left: 10px;"><i class="fas fa-trash-alt"></i> Remover</button>
+                          </div>
+                        </div>
+                    </fieldset>
 
+                    <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9;">
                       <div class="form-group col-md-5">
                         <label for="" style="color: #4e4e4e;"><b>Profissional(s) Disponível:</b></label>
                         <br>
@@ -99,12 +104,13 @@
                           <option value="3">Three</option>
                         </select>
                       </div>
-                    </div>
+                    </fieldset>
                   </div>
                 </div>
-                <div class="text-right">
-                  <button href="/agendamento/" type="submit" name="salvar" class="btn-lg btn" style="border: none;background-color: #82a4ef;color: white;">Salvar</button>
-                </div>
+              </div>
+              <div class="text-right">
+                <button href="/agendamento/" type="submit" name="salvar" class="btn-lg btn" style="border: none;background-color: #82a4ef;color: white;">Salvar</button>
+              </div>
             </form>
           </div>
         </div>
@@ -124,30 +130,30 @@
 </div>
 
 <script>
-    var maxButtons = 9999; 
+  var maxButtons = 9999;
 
-    $('.add_novo_Servico').click(function(e) {
-        var container = $(this).closest('.row');
-        var element_copy = container.clone();
-        var botao_excluir = '<div class="btn btn-outline-danger botao_remover" style="width: 100px; height: 40px; font-size: 14px; margin-top: 10px;"><i class="fas fa-trash-alt"></i> Remover</div>';
+  $('.add_novo_Servico').click(function(e) {
+    var container = $(this).closest('.row');
+    var element_copy = container.clone();
+    var botao_excluir = '<div class="btn btn-outline-danger botao_remover" style="width: 100px; height: 40px; font-size: 14px; margin-top: 10px;"><i class="fas fa-trash-alt"></i> Remover</div>';
 
-        element_copy.find('select, input').val('');
-        element_copy.find('.select2').remove(); // Limpa o campo
-        element_copy.find('.add_novo_Servico').remove(); // Remove o botão "Adicionar Serviço"
+    element_copy.find('select, input').val('');
+    element_copy.find('.select2').remove(); // Limpa o campo
+    element_copy.find('.add_novo_Servico').remove(); // Remove o botão "Adicionar Serviço"
 
-        container.after(element_copy);
+    container.after(element_copy);
 
-        if (element_copy.find('.botao_remover').length === 0) {
-            element_copy.find('.col-md-3').append(botao_excluir);
-        }
+    if (element_copy.find('.botao_remover').length === 0) {
+      element_copy.find('.col-md-3').append(botao_excluir);
+    }
 
-        $('.botao_remover').click(function(e) {
-            $(this).closest('.row').remove();
-        });
-
-        // Select picker
-        $('.servico').select2({
-            width: '100%'
-        });
+    $('.botao_remover').click(function(e) {
+      $(this).closest('.row').remove();
     });
+
+    // Select picker
+    $('.servico').select2({
+      width: '100%'
+    });
+  });
 </script>

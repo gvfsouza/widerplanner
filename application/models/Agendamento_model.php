1d97d592e5297ed7ac894f6ffb1795be340b6e88
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-27 15:39:44 -0300

    .

--- application/models/Agendamento_model.php
+++ application/models/Agendamento_model.php
@@ -22,7 +22,7 @@ class Agendamento_model extends CI_Model
     {
         $data = array(
             'fk_agenda' => $fk_agenda,
-            'fk_servico' => $fk_servicos,
+            'fk_servicos' => $fk_servicos,
         );
 
         $this->db->insert('agenda2', $data);

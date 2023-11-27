5216ed9ed66c30cdc373b90900a0c08d01b46dde
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-26 22:40:29 -0300

    .

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -37,14 +37,13 @@
         <!-- Dropdown - User Information -->
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="javascript:void(0);" onclick="openLogoutModal()">
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-900"></i>
                 Sair
             </a>
         </div>
     </li>
 </ul>
-
 
 
         

44e42b5fea46885e008e9e963c7ae6a3546fc48c
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-27 09:29:04 -0300

    .

--- application/controllers/Cadastro_funcionario.php
+++ application/controllers/Cadastro_funcionario.php
@@ -2,34 +2,39 @@
 
 class Cadastro_funcionario extends CI_Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
-		// Carregue o arquivo functions_helper
-		$this->load->helper('functions_helper');
-		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-			$_POST = $this->clean_post_data($_POST);
-		}
-		$this->load->database();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('functions_helper');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $_POST = $this->clean_post_data($_POST);
+        }
+        $this->load->database();
+    }
 
-	private function clean_post_data($data)
-	{
-		foreach ($data as $key => $value) {
-			$data[$key] = anti_injection($value);
-		}
-		return $data;
-	}
+    private function clean_post_data($data)
+    {
+        foreach ($data as $key => $value) {
+            $data[$key] = anti_injection($value);
+        }
+        return $data;
+    }
 
-	public function index()
-	{
-		$this->load->model('Funcionario_model');
-		$dados = array();
+    public function index()
+    {
+        $this->load->model('Funcionario_model');
+        $dados = array();
 
-		if (isset($_POST['salvar'])) {
-			$foto_usuario = $this->converte_img($_FILES['foto_usuario']['tmp_name'], $_FILES['foto_usuario']['type']);
-			$nome_usuario = $this->input->post('nome_usuario');
-			$cpf_usuario = $this->input->post('cpf_usuario');
+        if ($this->input->post('salvar')) {
+            if (isset($_FILES['foto_usuario']) && $_FILES['foto_usuario']['error'] === UPLOAD_ERR_OK) {
+                $foto_usuario = $this->converte_img($_FILES['foto_usuario']['tmp_name'], $_FILES['foto_usuario']['type']);
+            } else {
+                $foto_usuario = '';
+            }
+
+            // Other POST variables
+            $nome_usuario = $this->input->post('nome_usuario');
+            $cpf_usuario = $this->input->post('cpf_usuario');
 			$dt_nasc_usuario = $this->input->post('dt_nasc_usuario');
 			$email_usuario = $this->input->post('email_usuario');
 			$sexo_usuario = $this->input->post('sexo_usuario');
@@ -41,46 +46,44 @@ class Cadastro_funcionario extends CI_Controller
 			$bairro_usuario = $this->input->post('bairro_usuario');
 			$cidade_usuario = $this->input->post('cidade_usuario');
 			$estado_usuario = $this->input->post('estado_usuario');
-			$fk_servicos = $this->input->post('fk_servicos');
+            $fk_servicos = $this->input->post('fk_servicos');
 
-			if (!empty($fk_servicos)) {
-				$dados['cadastro_funcionario'] = $this->Funcionario_model->cadastro_profissional($foto_usuario,	$nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario, $complemento_usuario, $bairro_usuario, $cidade_usuario, $estado_usuario, $fk_servicos, md5(gerarSenhaAleatoria(8)));
+            if (!empty($fk_servicos)) {
+                $dados['cadastro_funcionario'] = $this->Funcionario_model->cadastro_profissional($foto_usuario, $nome_usuario, $cpf_usuario, $dt_nasc_usuario, $email_usuario, $sexo_usuario, $telefone_usuario, $cep_usuario, $logradouro_usuario, $numero_usuario,$complemento_usuario, $bairro_usuario, $cidade_usuario, $estado_usuario, $fk_servicos, md5(gerarSenhaAleatoria(8)));
 
-				// Captura o ID do usuário recém-cadastrado
-				$fk_usuario = $dados['cadastro_funcionario'];
+                $fk_usuario = $dados['cadastro_funcionario'];
 
-				// Insere na tabela func_servicos
-				$this->Funcionario_model->associar_servico($fk_usuario, $fk_servicos);
+                $this->Funcionario_model->associar_servico($fk_usuario, $fk_servicos);
 
-				// MENSAGEM SUCESSO AO CADASTRAR
-				$this->session->set_flashdata('sucesso', 'Cadastro realizado com sucesso!');
-				redirect('cadastro_funcionario/');
-			} else {
-				$this->session->set_flashdata('erro', 'Selecione pelo menos um serviço antes de cadastrar.');
-			}
-		}
+                $this->session->set_flashdata('sucesso', 'Cadastro realizado com sucesso!');
+                redirect('cadastro_funcionario/');
+            } else {
+                $this->session->set_flashdata('erro', 'Selecione pelo menos um serviço antes de cadastrar.');
+            }
+        }
 
-		$dados['listar_servicos'] = $this->Funcionario_model->listar_servicos();
+        $dados['listar_servicos'] = $this->Funcionario_model->listar_servicos();
 
-		$this->load->view('layout/header');
-		$this->load->view('layout/sidebar');
-		$this->load->view('layout/navbar');
-		$this->load->view('cadastro_funcionario', $dados);
-	}
+        $this->load->view('layout/header');
+        $this->load->view('layout/sidebar');
+        $this->load->view('layout/navbar');
+        $this->load->view('cadastro_funcionario', $dados);
+    }
 
-	public function converte_img($img, $type)
-	{
-		if ($type == 'image/png') {
-			$im = imagecreatefrompng($img);
-			ob_start();
-			imagejpeg($im);
-			$data = ob_get_clean();
-			imagedestroy($im);
-		} else {
-			ob_start();
-			readfile($img);
-			$data = ob_get_clean();
-		}
-		return base64_encode($data);
-	}
+    public function converte_img($img, $type)
+    {
+        if ($type == 'image/png') {
+            $im = imagecreatefrompng($img);
+            ob_start();
+            imagejpeg($im);
+            $data = ob_get_clean();
+            imagedestroy($im);
+        } else {
+            ob_start();
+            readfile($img);
+            $data = ob_get_clean();
+        }
+        return base64_encode($data);
+    }
 }
+

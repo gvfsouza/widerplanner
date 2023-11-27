c612519088786cc945fd0173b6913003f4b9bc41
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-27 17:58:02 -0300

    .

--- application/views/login.php
+++ application/views/login.php
@@ -102,12 +102,12 @@
 
                                     <?php
                                     if ($this->session->flashdata('error')) {
-                                        echo '<div style="background-color: #F8D7DA; color: #6a040d; padding: 10px; width: 318px; height: 50px; vertical-align: middle; border-radius: 5px;">';
+                                        echo '<div style="background-color: #F8D7DA; color: #6a040d; padding: 10px; height: 50px; vertical-align: middle; border-radius: 5px;">';
                                         echo $this->session->flashdata('error');
                                         echo '</div>';
                                     }
                                     ?>
-
+                                    <br>
                                     <form method="POST" action="<?php echo site_url('login/login'); ?>" name="form_login" class="login">
                                     <div class="form-group">
                                             <label for="cpf_usuario" style="color: #a8aaad"> CPF:</label>

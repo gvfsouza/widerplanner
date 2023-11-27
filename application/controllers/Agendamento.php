ba3ab13976f3e2722d8c28924603117994756f3b
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-27 12:02:05 -0300

    .

--- application/controllers/Agendamento.php
+++ application/controllers/Agendamento.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Cadastro_funcionario extends CI_Controller
+class Agendamento extends CI_Controller
 {
 	public function __construct()
 	{

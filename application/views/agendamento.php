1b6f99396258f0500f4dff879e0b8c3753def7d3
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-27 12:14:32 -0300

    .

--- application/views/agendamento.php
+++ application/views/agendamento.php
@@ -45,7 +45,7 @@
                 <div class="row">
                   <div class="col-12">
                     <div class="row">
-                      <div class="form-group col-md-6">
+                      <div class="form-group col-md-3">
                         <label for="" style="color: #4e4e4e;"><b>Escolha um dia da Semana:</b></label>
                         <br>
                         <select name="fk_dia_semana" id="fk_dia_semana" class="form-control fk_dia_semana" style="cursor: pointer;" required>
@@ -56,7 +56,7 @@
                         </select>
                       </div>
 
-                      <div class="form-group col-md-6">
+                      <div class="form-group col-md-3">
                         <label for="" style="color: #4e4e4e;"><b>Escolha um Horário:</b></label>
                         <br>
                         <select name="horaDisp" id="horaDisp" class="form-control" style="cursor: pointer;" required>

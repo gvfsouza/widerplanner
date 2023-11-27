24d847197dab74d939ce1784bc48f7565af3329d
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-27 16:26:31 -0300

    .

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -4,41 +4,26 @@
 class Login_model extends CI_Model
 {
     // Login
-    // public function autenticacao_usuario($cpf_usuario, $senha)
-    // {
-    //     $senha = md5($senha);
-
-    //     // Realiza a autenticação do usuário, por exemplo, consulta no banco de dados
-    //     $this->db->where('cpf_usuario', $cpf_usuario);
-    //     $this->db->where('senha', $senha);
-    //     $query = $this->db->get('usuario');
-
-    //     if ($query->num_rows() == 1) {
-    //         return $query->row();
-    //     } else {
-    //         return FALSE;
-    //     }
-
-    //     // return $this->db->get()->row_array();
-    // }
-
     public function autenticacao_usuario($cpf_usuario, $senha)
     {
+        $senha = md5($senha);
+
+        // Realiza a autenticação do usuário, por exemplo, consulta no banco de dados
         $this->db->where('cpf_usuario', $cpf_usuario);
         $this->db->where('senha', $senha);
         $query = $this->db->get('usuario');
 
         if ($query->num_rows() == 1) {
-            $user = $query->row();
+            return $query->row();
 
-            // Retorne os dados do usuário, incluindo o fk_usuario
             return array(
                 'fk_usuario' => $user->fk_usuario,
-                'cpf_usuario' => $user->cpf_usuario,
             );
         } else {
-            return false;
+            return FALSE;
         }
+
+        // return $this->db->get()->row_array();
     }
 
     public function salvar_dados_sessao($cpf_usuario)

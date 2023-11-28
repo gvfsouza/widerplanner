d42816ac135fd2628b778a682b8370048e70dcbb
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-28 09:28:50 -0300

    .

--- application/controllers/Agendamento.php
+++ application/controllers/Agendamento.php
@@ -70,7 +70,7 @@ class Agendamento extends CI_Controller
 		}
 
 		// Verifique se já existe agendamento
-		$existe_agendamento = verificar_agendamento($data_agenda, $fk_profissional);
+		$existe_agendamento = $this->Agendamento_model->verificar_agendamento($data_agenda, $fk_profissional);
 
 		// Se não existir agendamento, liste os horários disponíveis
 		if (!$existe_agendamento) {

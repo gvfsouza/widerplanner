edaba58170e3ef462539c8e4d3e51d293c5f6800
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-29 11:55:46 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -3,20 +3,6 @@
         color: black;
     }
 
-    /* .card-profile {
-        width: 350px;
-        display: flex;
-        
-        flex-direction: column;
-        align-items: center;
-        padding: 2rem;
-        background-color: white;
-        border-radius: 1rem;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-        margin: 12px;
-        margin-top: 40px;
-    } */
-
     .profile-image {
         position: relative;
         top: -70px;
@@ -45,6 +31,11 @@
     .profile-info>.profile-desc {
         color: #666666;
         font-size: 0.9rem;
+    }
+
+    .profile-info>.profille-desc {
+        color: #666666;
+        font-size: 0.7rem;
     }
 
     .status-value {

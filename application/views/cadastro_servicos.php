f7cce9bae409283129483ab523369b60408a84d8
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-29 09:23:50 -0300

    .

--- application/views/cadastro_servicos.php
+++ application/views/cadastro_servicos.php
@@ -41,7 +41,7 @@
                                                     <div class="form-group col-md-3">
                                                         <label for=""><b>Foto:</b></label>
                                                         <br>
-                                                        <input style="font-size:14px" type="file" class="form-control arquivo" id="foto_servico" name="foto_servico" accept="image/*" required>
+                                                        <input style="font-size:14px" type="file" class="form-control arquivo" id="foto_servico" name="foto_servico" accept="image/*">
 
                                                         <div class="foto_servico col mb-3">
                                                             <div class="foto_servico"></div>

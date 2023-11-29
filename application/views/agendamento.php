ab98a1a670b610051c314a244e40f93c334e04df
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-29 12:53:56 -0300

    .

--- application/views/agendamento.php
+++ application/views/agendamento.php
@@ -117,7 +117,7 @@
                                 </div>
                             </div>
                             <div class="text-right">
-                                <button href="<?php echo base_url(); ?>agendamento/" type="submit" name="salvar" class="btn-lg btn" style="border: none;background-color: #82a4ef;color: white;">Salvar</button>
+                                <button href="<?php echo base_url(); ?>agendamento/" type="submit" name="salvar" class="btn-lg btn" style="border: none;background-color: #82a4ef;color: white;" value="1">Salvar</button>
                             </div>
                         </form>
                     </div>

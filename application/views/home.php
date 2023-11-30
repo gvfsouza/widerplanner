9ad673da267a234c14d65b597d48f56f1cc6c8b0
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-30 08:28:59 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -325,8 +325,8 @@
                                                                         </div>
                                                                         <div>
                                                                             <h3><?php echo $value->nome_usuario; ?></h3>
-                                                                            <p><i class="fa-solid fa-user"></i><?php echo $value->descricao_usuario; ?></p>
-                                                                            <p><i class="fas fa-solid fa-paper-plane"></i><?php echo $value->email_usuario; ?></p>
+                                                                            <p><?php echo $value->descricao_usuario; ?></p>
+                                                                            <p><i class="fas fa-solid fa-paper-plane"></i>&nbsp;<?php echo $value->email_usuario; ?></p>
                                                                         </div>
                                                                     </div>
                                                                 <?php endif; ?>

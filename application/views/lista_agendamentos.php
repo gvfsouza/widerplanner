d3693cb8ea347112f325a3626cea740b36e1c8f4
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-30 16:53:37 -0300

    .

--- application/views/lista_agendamentos.php
+++ application/views/lista_agendamentos.php
@@ -192,18 +192,19 @@
                             </thead>
                             <tbody>
                             <?php foreach ($listar_agendamentos_realizados as $value) { ?>
-                                <tr>
-                                <th scope="row"><?php echo $value->nome_usuario; ?></th>
-                                <td><?php echo formata_data($value->data_agenda); ?></td>
-                                <td><?php echo $value->horarios_semana; ?></td>
-                            <?php } ?>
-                            <?php if (!empty($listar_profissionais_agendamentos)) { ?>
-                                <td><?php echo $listar_profissionais_agendamentos[0]->nome_usuario; ?></td>
+                                    <tr>
+                                    <th scope="row"><?php echo $value->nome_usuario; ?></th>
+                                    <td><?php echo formata_data($value->data_agenda); ?></td>
+                                    <td><?php echo $value->horarios_semana; ?></td>
+                                <?php if (!empty($listar_profissionais_agendamentos)) { ?>
+                                        <td><?php echo $listar_profissionais_agendamentos[0]->nome_usuario; ?></td>
+                                <?php } ?>
+
+                                <?php if (!empty($listar_servicos_agendamentos)) { ?>
+                                        <td><?php echo $listar_servicos_agendamentos[0]->nome_servico; ?></td>
+                                <?php } ?>
                             <?php } ?>
 
-                            <?php if (!empty($listar_servicos_agendamentos)) { ?>
-                                <td><?php echo $listar_servicos_agendamentos[0]->nome_servico; ?></td>
-                            <?php } ?>
                             </tbody>
                         </table>
                     </div>

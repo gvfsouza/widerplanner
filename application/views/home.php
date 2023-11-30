4bd5b45432d4b793f9d58e7ca007dff44492cc5e
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-30 13:31:05 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -380,7 +380,7 @@
                                                                 <div class="card usuario" style="width: 100%;">
                                                                     <div class="img">
                                                                         <?php if (!empty($value->foto_produto)) { ?>
-                                                                            <center><img style='display:block; width:100px;height:100px;' id='base64image' src='data:image/jpeg;base64,<?php echo $value->foto_produto ?>' /></center>
+                                                                            <center><img style="width:130px; height:150px" id='base64image' src='data:image/jpeg;base64,<?php echo $value->foto_produto ?>' /></center>
                                                                         <?php } else { ?>
                                                                             <center><img src="https://scontent-gru2-2.xx.fbcdn.net/v/t39.30808-6/302135953_502845391841930_8585948991976360589_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=TpFICO55swEAX9n1EQ0&_nc_ht=scontent-gru2-2.xx&oh=00_AfBT9HRfTe5yRcAsGZt3sDXK85JhzGBXzs-N63KIrkrQ-A&oe=656B55E5" alt="Logo Barbearia"></center>
                                                                         <?php } ?>

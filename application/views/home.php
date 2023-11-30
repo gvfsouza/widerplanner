a4d6ad78e29148fbc58d0c875b17dcd398ce9dc4
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-30 15:52:46 -0300

    ,.

--- application/views/home.php
+++ application/views/home.php
@@ -359,7 +359,7 @@
                                                                         <h3><?php echo $value->nome_produto; ?></h3>
                                                                         <p class="profile-desc" ><?php echo $value->descricao_produto; ?></p>
                                                                         <center>
-                                                                            <p class="status-value" style="color: #879BC9">R$ &nbsp;<b><?php echo $value->valor_produto; ?>,00</b></p>
+                                                                            <p class="status-value" style="color: #879BC9; font-weight: bold;">R$ &nbsp;<b><?php echo $value->valor_produto; ?>,00</b></p>
                                                                         </center>
                                                                     </div>
                                                                 </div>

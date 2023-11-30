bd90b7261ab8566187f1a72f2feadc4df19dcd0f
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-11-30 08:42:32 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -304,7 +304,7 @@
                                             </div>
                                         </fieldset>
 
-                                        <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9; display: flex; flex-wrap: wrap; justify-content: space-around;">
+                                        <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 15px; border: 1px solid #879bc9; display: flex; flex-wrap: wrap;">
                                             <?php if (!empty($listar_dados_estabelecimento)) : ?>
                                                 <?php foreach ($listar_dados_estabelecimento as $value) : ?>
                                                     <div class="card-container" style="flex: 0 0 300px; margin: 10px;">

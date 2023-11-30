e5d3835ec0296598b2fe074e26bd2816dad390a7
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-11-30 16:29:28 -0300

    .

--- application/controllers/Lista_agendamentos.php
+++ application/controllers/Lista_agendamentos.php
@@ -30,7 +30,6 @@ class Lista_agendamentos extends CI_Controller
 		$dados['listar_agendamentos_realizados'] = $this->Agendamento_model->listar_agendamentos_realizados();
 		$dados['listar_servicos_agendamentos'] = $this->Agendamento_model->listar_servicos_agendamentos();
 
-		
 		$this->load->view('layout/header');
 		$this->load->view('layout/sidebar');
 		$this->load->view('lista_agendamentos',$dados);

19e8eb539833b708dc159dd59453b6177e8286eb
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-01 13:24:19 -0300

    .

--- application/controllers/Perfil_cliente.php
+++ application/controllers/Perfil_cliente.php
@@ -1,8 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Perfil_cliente extends CI_Controller {
-	
+class Perfil_cliente extends CI_Controller
+{
+
 	public function __construct()
 	{
 		parent::__construct();
@@ -21,45 +22,46 @@ class Perfil_cliente extends CI_Controller {
 		}
 		return $data;
 	}
-	
+
 	public function index()
-{
-    $this->load->model('Cliente_model');
+	{
+		$this->load->model('Cliente_model');
 
-    $cpf_usuario = $this->session->userdata('cpf_usuario');
+		$cpf_usuario = $this->session->userdata('cpf_usuario');
 
-    if ($this->input->post('salvar')) {
-        $telefone_usuario = $this->input->post('telefone_usuario');
-        $cep_usuario = $this->input->post('cep_usuario');
-        $numero_usuario = $this->input->post('numero_usuario');
-        $complemento_usuario = $this->input->post('complemento_usuario');
+		if ($this->input->post('salvar')) {
+			$telefone_usuario = $this->input->post('telefone_usuario');
+			$cep_usuario = $this->input->post('cep_usuario');
+			$numero_usuario = $this->input->post('numero_usuario');
+			$complemento_usuario = $this->input->post('complemento_usuario');
 
-        if (!isset($error)) {
-            $dados_cliente = $this->Cliente_model->dados_cliente($cpf_usuario);
-            $this->Cliente_model->editar_dados_pesoais(
-                $dados_cliente->id_usuario,
-                $telefone_usuario,
-                $cep_usuario,
-                $numero_usuario,
-                $complemento_usuario
-            );
+			if (!isset($error)) {
+				$dados_cliente = $this->Cliente_model->dados_cliente($cpf_usuario);
+				$this->Cliente_model->editar_dados_pesoais(
+					$dados_cliente->id_usuario,
+					$telefone_usuario,
+					$cep_usuario,
+					$numero_usuario,
+					$complemento_usuario
+				);
 
-            $this->session->set_flashdata('sucesso', 'Alterações efetuadas com sucesso!');
-            redirect('cadastro_cliente');
-        } else {
-            $this->session->set_flashdata('erro', 'Erro ao atualizar os dados.');
-        }
-    }
+				$this->session->set_flashdata('sucesso', 'Alterações efetuadas com sucesso!');
+				redirect('cadastro_cliente');
+			} else {
+				$this->session->set_flashdata('erro', 'Erro ao atualizar os dados.');
+			}
+		}
 
-    $dados['dados_cliente'] = $this->Cliente_model->dados_cliente($cpf_usuario);
-    $dados['historico_agendamentos_realizados'] = $this->Cliente_model->historico_agendamentos_realizados();
-    $dados['historico_profissionais_agendamentos'] = $this->Cliente_model->historico_profissionais_agendamentos();
-    $dados['historico_servicos_agendamentos'] = $this->Cliente_model->historico_servicos_agendamentos();
+		$dados['dados_cliente'] = $this->Cliente_model->dados_cliente($cpf_usuario);
+		$dados['historico_agendamentos_realizados'] = $this->Cliente_model->historico_agendamentos_realizados();
+		$dados['historico_profissionais_agendamentos'] = $this->Cliente_model->historico_profissionais_agendamentos();
+		$dados['historico_servicos_agendamentos'] = $this->Cliente_model->historico_servicos_agendamentos();
 
-    $this->load->view('layout/header');
-    $this->load->view('layout/sidebar');
-    $this->load->view('layout/navbar');
-    $this->load->view('perfil_cliente', $dados);
-    $this->load->view('layout/footer');
-}
+		$this->load->view('layout/header');
+		$this->load->view('layout/sidebar');
+		$this->load->view('layout/navbar');
+		$this->load->view('perfil_cliente', $dados);
+		$this->load->view('layout/footer');
+	}
+
 }

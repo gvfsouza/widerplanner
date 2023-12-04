4a394248495a727bed568a30daa765e9ab1f8d3c
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-04 11:29:50 -0300

    .

--- application/views/fotos_barbearia.php
+++ application/views/fotos_barbearia.php
@@ -105,7 +105,7 @@
                                                     <div class="col">
                                                         <?php if (!empty($value->fotos_lugar)) : ?>
                                                             <div class="img-placeholder">
-                                                                <center><img style='display:block; width:100px;height:100px;' id='base64image' src='data:image/jpeg;base64,<?php echo $value->fotos_lugar ?>' /></center>
+                                                                <center><img style='display:block; width:100px;height:100px;' id='base64image' src='data:image/jpeg;base64,<?php echo $value->fotos_lugar; ?>' /></center>
                                                             </div>
                                                         <?php endif; ?>
                                                     </div>

278b59ee9bb6af077c42f2b9876d55a26e4220d1
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-04 13:57:42 -0300

    .

--- application/views/fotos_barbearia.php
+++ application/views/fotos_barbearia.php
@@ -65,7 +65,7 @@
                                         <div class="row">
                                             <?php if (!empty($listar_fotos)) : ?>
                                                 <?php foreach ($listar_fotos as $value) : ?>
-                                                    <div class="col-md-4 mb-3">
+                                                    <div class="col">
                                                         <?php if (!empty($value->fotos_lugar)) : ?>
                                                             <div>
                                                                 <center><img id='base64image' src='data:image/jpeg;base64,<?php echo $value->fotos_lugar; ?>' /></center>

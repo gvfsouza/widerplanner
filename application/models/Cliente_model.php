a1f81a100ddcadc214e0da8e90dc38accda50bde
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-04 16:19:14 -0300

    .

--- application/models/Cliente_model.php
+++ application/models/Cliente_model.php
@@ -77,9 +77,9 @@ class Cliente_model extends CI_Model
     public function historico_servicos_agendamentos()
     {
         $this->db->select('agenda.id_agenda, GROUP_CONCAT(servicos.nome_servico) as servicos');
-        $this->db->from('agenda2');
+        $this->db->from('agenda');
+        $this->db->join('agenda2', 'agenda.id_agenda = agenda2.fk_agenda', 'left');
         $this->db->join('servicos', 'servicos.id_servicos = agenda2.fk_servicos', 'left');
-        $this->db->join('agenda', 'agenda.id_agenda = agenda2.fk_agenda', 'left');
         $this->db->group_by('agenda.id_agenda');
 
         $res = $this->db->get();

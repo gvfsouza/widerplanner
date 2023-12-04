296341af23cf7c6c9fa4a987c710e2bd223bb24b
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-04 13:21:34 -0300

    .

--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -84,7 +84,7 @@
                                                         <div class="h5 mb-0 font-weight-bold text-gray-800">quantidade</div>
                                                     </div>
                                                     <div class="col-auto">
-                                                        <i class="fas fa-syringe fa-2x text-gray-300"></i>
+                                                        <i class="fas fa-users"></i>
                                                     </div>
                                                 </div>
                                             </div>

cbeb3a9dc1c87e76334e0517d8ebe56df63aadda
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-04 10:07:58 -0300

    .

--- application/views/fotos_barbearia.php
+++ application/views/fotos_barbearia.php
@@ -71,7 +71,7 @@
                                         <br>
                                         <input style="font-size:14px" type="file" class="form-control" id="fotos_lugar" name="fotos_lugar" accept="image/*">
                                     </div>
-                                    <div class="form-group col text-right">
+                                    <div class="form-group col">
                                         <label for="" class="invisible"></label>
                                         <br>
                                         <button href="/fotos_barbearia/" type="submit" name="salvar" class="btn-lg btn" style="border: none; background-color: #82a4ef; color: white;">Salvar</button>

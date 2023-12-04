f0048ed8d2f074d0a3db1f780f1865a6c8d46704
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-04 13:06:13 -0300

    .

--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -99,33 +99,46 @@
 <script>
             var options = {
           series: [{
-          name: 'Marine Sprite',
-          data: [44, 55, 41, 37, 22, 43, 21]
+          name: 'PRODUCT A',
+          data: [44, 55, 41, 67, 22, 43]
         }, {
-          name: 'Striking Calf',
-          data: [53, 32, 33, 52, 13, 43, 32]
+          name: 'PRODUCT B',
+          data: [13, 23, 20, 8, 13, 27]
         }, {
-          name: 'Tank Picture',
-          data: [12, 17, 11, 9, 15, 11, 20]
+          name: 'PRODUCT C',
+          data: [11, 17, 15, 15, 21, 14]
         }, {
-          name: 'Bucket Slope',
-          data: [9, 7, 5, 8, 6, 9, 4]
-        }, {
-          name: 'Reborn Kid',
-          data: [25, 12, 19, 32, 25, 24, 10]
+          name: 'PRODUCT D',
+          data: [21, 7, 25, 13, 22, 8]
         }],
           chart: {
           type: 'bar',
           height: 350,
           stacked: true,
+          toolbar: {
+            show: true
+          },
+          zoom: {
+            enabled: true
+          }
         },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: 'bottom',
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }],
         plotOptions: {
           bar: {
-            horizontal: true,
+            horizontal: false,
+            borderRadius: 10,
             dataLabels: {
               total: {
                 enabled: true,
-                offsetX: 0,
                 style: {
                   fontSize: '13px',
                   fontWeight: 900
@@ -134,40 +147,18 @@
             }
           },
         },
-        stroke: {
-          width: 1,
-          colors: ['#fff']
-        },
-        title: {
-          text: 'Fiction Books Sales'
-        },
         xaxis: {
-          categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
-          labels: {
-            formatter: function (val) {
-              return val + "K"
-            }
-          }
+          type: 'datetime',
+          categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+            '01/05/2011 GMT', '01/06/2011 GMT'
+          ],
         },
-        yaxis: {
-          title: {
-            text: undefined
-          },
-        },
-        tooltip: {
-          y: {
-            formatter: function (val) {
-              return val + "K"
-            }
-          }
+        legend: {
+          position: 'right',
+          offsetY: 40
         },
         fill: {
           opacity: 1
-        },
-        legend: {
-          position: 'top',
-          horizontalAlign: 'left',
-          offsetX: 40
         }
         };
 

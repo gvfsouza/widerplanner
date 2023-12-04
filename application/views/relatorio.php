f17d9562e53a832795a5ca60efae1b35596633f9
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-04 11:50:36 -0300

    .

--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -1,1 +1,83 @@
-<h1>teste</h1>
+<style>
+  body {
+    display: grid;
+    min-height: 100vh;
+  }
+
+  figure {
+    width: 500px;
+    aspect-ratio: 7 / 4;
+    --bg: hsl(330 80% calc(90% - (var(--hover) * 10%)));
+    /* --accent: hsl(280 80% 40%); */
+    transition: background 0.2s;
+    /* background:
+      radial-gradient(circle at top left, var(--accent), transparent 75%),
+      var(--bg); */
+    margin: 0;
+    position: relative;
+    overflow: hidden;
+    border-radius: 1.5rem;
+    margin: 10px;
+  }
+
+  figure:after {
+    content: "";
+    position: absolute;
+    border-radius: 50%;
+    bottom: 0%;
+    left: 10%;
+  }
+
+  img {
+    position: absolute;
+  }
+
+  article {
+    --hover: 0;
+  }
+
+  article:hover {
+    --hover: 1;
+  }
+</style>
+
+<br>
+
+<div>
+    <div class="container-fluid">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3" style="background-color: white;">
+                <div>
+                <h3 id="dados-agenda" style="font-size: 25px;font-weight:bold"></h3>
+                <br>
+                </div>
+
+                <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                <div>
+                    <legend class="form-group" style="color: white; font-size: 18px;">
+                    <strong>Relatório</strong>
+                    </legend>
+                </div>
+                </fieldset>
+
+                <!-- FORMULÁRIO  -->
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="" role="tabpanel">
+                        <div class="p-3 bg-white">
+                           
+                        </div>
+                    </div>
+                    <br><br>
+                    <div class="col-12 text-center">
+                        <span style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
+                            Projeto de Conclusão de Curso - Superior de Tecnologia em Análise e Deselvolvimento de Sistemas
+                        <br>
+                            Centro Universitário Nossa Senhora do Patrocínio <br>
+                            Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
+                    </div>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

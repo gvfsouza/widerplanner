d0f4aad42dc93a96663d929a20c064764de053c0
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-04 16:09:10 -0300

    .

--- application/models/Relatorio_model.php
+++ application/models/Relatorio_model.php
@@ -5,6 +5,7 @@ model relatório
 
 class Relatorio_model extends CI_Model
 {
+    // Card usuários
     public function listar_usuarios_cadastrados()
     {
         $this->db->select("COUNT(usuario.id_usuario) as quantidade_usuarios");
@@ -13,10 +14,26 @@ class Relatorio_model extends CI_Model
         return $res->result();
     }
 
+    // Card Serviços
     public function total_servicos()
     {
         $this->db->select("COUNT(servicos.id_servicos) as quantidade_servicos");
         $this->db->from('servicos');
+        $res = $this->db->get();
+        return $res->result();
+    }
+
+    // Gráfico listar agendamentos por mês
+    public function listar_agendamentos_mes()
+    {
+        $this->db->select("COUNT(id_agenda) as quantidade, DATE_PART('MONTH',data_agenda) as mes, DATE_PART('YEAR',data_agenda) as ano");
+        $this->db->from('agenda');
+        $this->db->where("DATE_PART('MONTH',data_agenda) >", 0);
+        $ano_atual = date('Y'); // traz os registros do ano atual
+        $this->db->where("DATE_PART('YEAR',data_agenda) = ", $ano_atual);
+        $this->db->group_by("DATE_PART('MONTH',data_agenda)");
+        $this->db->group_by("DATE_PART('YEAR',data_agenda)");
+
         $res = $this->db->get();
         return $res->result();
     }
--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -1,99 +1,111 @@
 view relatório
 
 <style>
-  body {
-    display: grid;
-    min-height: 100vh;
-  }
+    body {
+        display: grid;
+        min-height: 100vh;
+    }
 
-  figure {
-    width: 500px;
-    aspect-ratio: 7 / 4;
-    --bg: hsl(330 80% calc(90% - (var(--hover) * 10%)));
-    /* --accent: hsl(280 80% 40%); */
-    transition: background 0.2s;
-    /* background:
+    figure {
+        width: 500px;
+        aspect-ratio: 7 / 4;
+        --bg: hsl(330 80% calc(90% - (var(--hover) * 10%)));
+        /* --accent: hsl(280 80% 40%); */
+        transition: background 0.2s;
+        /* background:
       radial-gradient(circle at top left, var(--accent), transparent 75%),
       var(--bg); */
-    margin: 0;
-    position: relative;
-    overflow: hidden;
-    border-radius: 1.5rem;
-    margin: 10px;
-  }
+        margin: 0;
+        position: relative;
+        overflow: hidden;
+        border-radius: 1.5rem;
+        margin: 10px;
+    }
 
-  figure:after {
-    content: "";
-    position: absolute;
-    border-radius: 50%;
-    bottom: 0%;
-    left: 10%;
-  }
+    figure:after {
+        content: "";
+        position: absolute;
+        border-radius: 50%;
+        bottom: 0%;
+        left: 10%;
+    }
 
-  img {
-    position: absolute;
-  }
+    img {
+        position: absolute;
+    }
 
-  article {
-    --hover: 0;
-  }
+    article {
+        --hover: 0;
+    }
 
-  article:hover {
-    --hover: 1;
-  }
+    article:hover {
+        --hover: 1;
+    }
 </style>
 
-<br>
+<br />
 
 <div>
     <div class="container-fluid">
         <div class="card shadow mb-4">
-            <div class="card-header py-3" style="background-color: white;">
+            <div class="card-header py-3" style="background-color: white">
                 <div>
-                <h3 id="dados-agenda" style="font-size: 25px;font-weight:bold"></h3>
-                <br>
+                    <h3 id="dados-agenda" style="font-size: 25px; font-weight: bold"></h3>
+                    <br />
                 </div>
 
-                <fieldset class="form-group" style="border: 1px solid #ddd; border-radius: 4px; padding: 7px; border: 1px solid #879bc9;background: #879bc9;color: black;">
+                <fieldset class="form-group" style="
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            padding: 7px;
+            border: 1px solid #879bc9;
+            background: #879bc9;
+            color: black;
+          ">
                     <div>
-                        <legend class="form-group" style="color: white; font-size: 18px;">
-                        <strong>Relatório</strong>
+                        <legend class="form-group" style="color: white; font-size: 18px">
+                            <strong>Relatório</strong>
                         </legend>
                     </div>
                 </fieldset>
 
-                <div class="row">
-                    <!-- Card Usuários -->
-                    <div class="col-xl-4 col-md-6 mb-3">
-                        <div class="card border-left-primary shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                            Usuários</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $listar_usuarios_cadastrados[0]->quantidade_usuarios; ?></div>
+                <!-- Card Usuários -->
+                <div class="col-xl-4 col-md-6 mb-3">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Usuários
                                     </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-users"></i>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        <?php echo $listar_usuarios_cadastrados[0]->quantidade_usuarios;
+                                        ?>
                                     </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-users"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
-                    <!-- Card serviços oferecidos -->
-                    <div class="col-xl-4 col-md-6 mb-3">
-                        <div class="card border-left-primary shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                            Total de Seviços</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $total_servicos[0]->quantidade_servicos; ?></div>
+                </div>
+
+                <!-- Card serviços oferecidos -->
+                <div class="col-xl-4 col-md-6 mb-3">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Total de Seviços
                                     </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-users"></i>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                        <?php echo $total_servicos[0]->quantidade_servicos; ?>
                                     </div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-users"></i>
                                 </div>
                             </div>
                         </div>
@@ -108,21 +120,23 @@ view relatório
                                 <!-- Gráfico -->
                                 <div class="col-md-5">
                                     <div class="card shadow mb-4">
-                                        <div id="chart" style="background-color:white;"></div>
+                                        <div id="chart" style="background-color: white"></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <br><br>
+                    <br /><br />
                     <div class="col-12 text-center">
-                        <span style="text-align: center">Sistema desenvolvido para cunho estudantil <br>
-                            Projeto de Conclusão de Curso - Superior de Tecnologia em Análise e Deselvolvimento de Sistemas
-                        <br>
-                            Centro Universitário Nossa Senhora do Patrocínio <br>
-                            Endereço: R. José Weissohn, 153 - Centro, Salto - SP, 13328-300</span>
+                        <span style="text-align: center">Sistema desenvolvido para cunho estudantil <br />
+                            Projeto de Conclusão de Curso - Superior de Tecnologia em Análise
+                            e Deselvolvimento de Sistemas
+                            <br />
+                            Centro Universitário Nossa Senhora do Patrocínio <br />
+                            Endereço: R. José Weissohn, 153 - Centro, Salto - SP,
+                            13328-300</span>
                     </div>
-                    <br>
+                    <br />
                 </div>
             </div>
         </div>
@@ -130,70 +144,91 @@ view relatório
 </div>
 
 <script>
-            var options = {
-          series: [{
-          name: 'PRODUCT A',
-          data: [44, 55, 41, 67, 22, 43]
-        }, {
-          name: 'PRODUCT B',
-          data: [13, 23, 20, 8, 13, 27]
-        }, {
-          name: 'PRODUCT C',
-          data: [11, 17, 15, 15, 21, 14]
-        }, {
-          name: 'PRODUCT D',
-          data: [21, 7, 25, 13, 22, 8]
-        }],
-          chart: {
-          type: 'bar',
-          height: 350,
-          stacked: true,
-          toolbar: {
-            show: true
-          },
-          zoom: {
-            enabled: true
-          }
+    var options = {
+        series: [{
+                name: "servico A",
+                data: [44, 55, 41, 67, 22, 43],
+            },
+            {
+                name: "servico B",
+                data: [13, 23, 20, 8, 13, 27],
+            },
+            {
+                name: "servico C",
+                data: [11, 17, 15, 15, 21, 14],
+            },
+            {
+                name: "servico d",
+                data: [21, 7, 25, 13, 22, 8],
+            },
+        ],
+        chart: {
+            type: "bar",
+            height: 350,
+            stacked: true,
+            toolbar: {
+                show: true,
+            },
+            zoom: {
+                enabled: true,
+            },
         },
         responsive: [{
-          breakpoint: 480,
-          options: {
-            legend: {
-              position: 'bottom',
-              offsetX: -10,
-              offsetY: 0
-            }
-          }
-        }],
+            breakpoint: 480,
+            options: {
+                legend: {
+                    position: "bottom",
+                    offsetX: -10,
+                    offsetY: 0,
+                },
+            },
+        }, ],
         plotOptions: {
-          bar: {
-            horizontal: false,
-            borderRadius: 10,
-            dataLabels: {
-              total: {
-                enabled: true,
-                style: {
-                  fontSize: '13px',
-                  fontWeight: 900
-                }
-              }
-            }
-          },
+            bar: {
+                horizontal: false,
+                borderRadius: 10,
+                dataLabels: {
+                    total: {
+                        enabled: true,
+                        style: {
+                            fontSize: "13px",
+                            fontWeight: 900,
+                        },
+                    },
+                },
+            },
         },
         xaxis: {
-        type: 'category',
-        categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT', '01/05/2011 GMT', '01/06/2011 GMT'],
+            type: "category",
+            categories: [
+                // mês
+                <?php
+                $meses = array(1 => 'Janeiro', 2 => 'Fevereiro', 3 => 'Março', 4 => 'Abril', 5 => 'Maio', 6 => 'Junho', 7 => 'Julho', 8 => 'Agosto', 9 => 'Setembro', 10 => 'Outubro', 11 => 'Novembro', 12 => 'Dezembro');
+
+                $i = 0;
+
+                foreach ($listar_agendamentos_mes as $value) {
+                    $i++;
+                ?> '<?php echo $meses[$value->mes]; ?>'
+                    <?php if ($i < count($listar_agendamentos_mes)) {
+                        echo ',';
+                    }
+                    ?>
+                <?php
+                }
+                ?>
+            ],
         },
 
         legend: {
-          position: 'right',
-          offsetY: 40
+            position: "right",
+            offsetY: 40,
         },
         fill: {
-          opacity: 1
-        }
-        };
+            opacity: 1,
+        },
+    };
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
 </script>

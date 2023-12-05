1a66ada063d1c8dd4453e03b3d58e51c91661a6e
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-05 11:10:02 -0300

    .

--- application/views/home.php
+++ application/views/home.php
@@ -277,6 +277,7 @@
                             <div class="p-3 bg-white">
                                 <div class="row">
                                     <div class="col-md-6">
+                                        <br>
                                         <img src="https://scontent-gru2-2.xx.fbcdn.net/v/t39.30808-6/302135953_502845391841930_8585948991976360589_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=TpFICO55swEAX9n1EQ0&_nc_ht=scontent-gru2-2.xx&oh=00_AfBT9HRfTe5yRcAsGZt3sDXK85JhzGBXzs-N63KIrkrQ-A&oe=656B55E5" alt="Logo Barbearia" style="width: 150px; height: 150px; border-radius: 15px">
                                     </div>
                                     <div class="col-md-6">

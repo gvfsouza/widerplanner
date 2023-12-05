bc378dca3eb2924749b74fb216496e8c3ee97da6
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-05 13:37:03 -0300

    .

--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -213,7 +213,7 @@
             type: "category",
             categories: [
                 // mês
-                <?php
+                 <?php
                 $meses = array(1 => 'Janeiro', 2 => 'Fevereiro', 3 => 'Março', 4 => 'Abril', 5 => 'Maio', 6 => 'Junho', 7 => 'Julho', 8 => 'Agosto', 9 => 'Setembro', 10 => 'Outubro', 11 => 'Novembro', 12 => 'Dezembro');
 
                 $i = 0;

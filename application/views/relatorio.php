60385a7735a3b44199841572bb2253924abf4496
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-06 08:55:01 -0300

    .

--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -76,11 +76,11 @@
                     </div>
                     <!-- Card serviços oferecidos -->
                     <div class="col-xl-4 col-md-6 mb-3">
-                        <div class="card border-left-primary shadow h-100 py-2" style="background-color:#af45ff">
+                        <div class="card border-left shadow h-100 py-2" style="background-color:#af45ff">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" style="color:#af45ff">
+                                        <div class="text-xs font-weight-bold text-uppercase mb-1" style="color:#af45ff">
                                             Total de Seviços
                                         </div>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">

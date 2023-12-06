71775401cc3e81ea2c7c2c7406e666a8e0e83316
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-06 15:10:44 -0300

    .

--- application/views/fotos_barbearia.php
+++ application/views/fotos_barbearia.php
@@ -70,12 +70,12 @@
                             <form action="" method="POST" enctype="multipart/form-data">
                                 <?php if ($this->session->fk_usuario != '' && $this->session->profissional == 'sim'): ?>
                                     <div class="row">
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-10">
                                             <label for="fotos_lugar"><b>Inserir uma Foto:</b></label>
                                             <input style="font-size: 14px" type="file" class="form-control" id="fotos_lugar" name="fotos_lugar" accept="image/*">
                                         </div>
 
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col">
                                             <br>
                                             <label class="invisible">&nbsp;</label>
                                             <button type="submit" name="salvar" class="btn-lg btn" style="border: none;background-color: #82a4ef;color: white;">Salvar</button>

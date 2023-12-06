8a5f441474446c56eff1155c3224dff4b715d6bf
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-06 08:04:50 -0300

    .

--- application/controllers/Cadastro_cliente.php
+++ application/controllers/Cadastro_cliente.php
@@ -6,6 +6,8 @@ class Cadastro_cliente extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+		if ($this->session->userdata('cpf_usuario') == null) redirect('login'); //direciona para a página de login quando perde a sessão
+
         // Carregue o arquivo functions_helper
         $this->load->helper('functions_helper');
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {

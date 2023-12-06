8c11456fc682dbea0bbc754a9432faf2643334da
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-06 16:16:10 -0300

    .

--- application/controllers/Lista_agendamentos.php
+++ application/controllers/Lista_agendamentos.php
@@ -35,6 +35,7 @@ class Lista_agendamentos extends CI_Controller
 
 		$this->load->view('layout/header');
 		$this->load->view('layout/sidebar');
+		$this->load->view('layout/navbar');
 		$this->load->view('lista_agendamentos',$dados);
 		$this->load->view('layout/footer');
 	}

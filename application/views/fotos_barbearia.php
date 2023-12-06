8235ef77b90c52b0a81c409ebf31c42e0ef818a7
Giovanna Souza <116668893+gvfsouza@users.noreply.github.com>
2023-12-06 08:28:26 -0300

    .

--- application/views/fotos_barbearia.php
+++ application/views/fotos_barbearia.php
@@ -60,27 +60,23 @@
                                 </div>
 
                                 <br><br>
-                                
+                            
                                 <div class="row">
-                                    <div class="col-12">
-                                        <div class="row">
-                                            <?php if (!empty($listar_fotos)) : ?>
-                                                <?php foreach ($listar_fotos as $value) : ?>
-                                                    <div class="col-md-4">
-                                                        <?php if (!empty($value->fotos_lugar)) : ?>
-                                                            <div>
-                                                                <center><img id='base64image' src='data:image/jpeg;base64,<?php echo base64_encode($value->fotos_lugar); ?>' /></center>
-                                                            </div>
-                                                        <?php endif; ?>
+                                    <?php if (!empty($listar_fotos)) : ?>
+                                        <?php foreach ($listar_fotos as $value) : ?>
+                                            <div class="col-md-4 mb-3">
+                                                <?php if (!empty($value->fotos_lugar)) : ?>
+                                                    <div>
+                                                        <center><img id='base64image' src='data:image/jpeg;base64,<?php echo base64_encode($value->fotos_lugar); ?>' /></center>
                                                     </div>
-                                                <?php endforeach; ?>
-                                            <?php else : ?>
-                                                <div class="alert alert-danger" role="alert">
-                                                    Nenhuma Foto Encontrada!
-                                                </div>
-                                            <?php endif; ?>
+                                                <?php endif; ?>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    <?php else : ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            Nenhuma Foto Encontrada!
                                         </div>
-                                    </div>
+                                    <?php endif; ?>
                                 </div>
                             </form>
                         </div>

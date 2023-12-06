a4b848728ced7b12b6bf532472ee2a4992d17637
kpsaram <128813360+kpsaram@users.noreply.github.com>
2023-12-06 08:53:45 -0300

    .

--- application/views/relatorio.php
+++ application/views/relatorio.php
@@ -80,7 +80,7 @@
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" style="color:#af45ff">
                                             Total de Seviços
                                         </div>
                                         <div class="h5 mb-0 font-weight-bold text-gray-800">
